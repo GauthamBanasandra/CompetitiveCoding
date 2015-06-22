3cd7d6568047a3103f81c22a81f00e46714717c7
Gautham B A <gautham.bangalore@gmail.com>
2015-06-22 20:21:16 +0530

    Some more.

--- Greedy/flowers.py
+++ Greedy/flowers.py
@@ -2,26 +2,28 @@ __author__ = 'gauth_000'
 import math
 
 n, k=map(int, str(input()).strip().split())
-c=sorted(map(int, str(input()).strip().split()))
-c_temp=[]
+flo=sorted(map(int, str(input()).strip().split()))
+print('sorted flo: ', flo)
+flo_cost=[]
 each=math.ceil(n/k)
-m=0
-while m<=k:
-    m+=each
-    for p in range(m-1, m-each-1, -1):
-        if p<len(c):
-            c_temp.append(c[p])
-
-print('c_temp: ', c_temp)
-# c=c_temp
-# i=0
-# cost=0
-# for person in range(k):
-#     for x in range(each):
-#         if i<n:
-#             try:
-#                 cost+=(x+1)*c[i]
-#                 i+=1
-#             except:
-#                 pass
-# print(cost)
+count=0
+for i in range(n):
+    count+=1
+    if count%each==0:
+        j=i
+        while count!=0:
+            flo_cost.append(flo[j])
+            j-=1
+            count-=1
+if len(flo)%each!=0:
+    for i in range(n-1, n-n%each-1, -1):
+        flo_cost.append(flo[i])
+print('flo_cost: ', flo_cost)
+flo=flo_cost
+x=0
+cost=0
+for i in range(n):
+    cost+=(x%each+1)*flo[i]
+    #print('cost: ', cost, ' x: ', x)
+    x+=1
+print(cost)
---
+++ Warmup/a_very_big_sum.py
@@ -0,0 +1,5 @@
+__author__ = 'gauth_000'
+
+input()
+numbers = map(int, str(input()).strip().split())
+print(sum(numbers))

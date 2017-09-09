8671d8402684fefb9afc323132bda3bda4a558da
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-09-09 20:31:34 +0530

    Add "Anagram"

---
+++ UVa/Ad hoc/Anagram/Anagram.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <ios>
+#include <algorithm>
+#include <string>
+
+#define MAX_ALPHA 200
+char alphabets[MAX_ALPHA];
+typedef unsigned long long big_int;
+
+bool Compare(char x, char y)
+{
+	return alphabets[x] < alphabets[y];
+}
+
+int main(int argc, char* argv[])
+{
+	std::ios::sync_with_stdio(false);
+
+	big_int test_cases;
+
+	for (int i = 'A', j = 0; i <= 'Z'; ++i)
+	{
+		alphabets[i] = j++;
+		alphabets[i + 32] = j++;
+	}
+
+	scanf("%llu\n", &test_cases);
+	while (test_cases--)
+	{		
+		char in[10000];
+		scanf("%s", in);
+		std::string word = in;
+		sort(word.begin(), word.end(), Compare);
+		do
+		{
+			printf("%s\n", word.c_str());
+		} while (next_permutation(word.begin(), word.end(), Compare));
+	}
+
+	return 0;
+}

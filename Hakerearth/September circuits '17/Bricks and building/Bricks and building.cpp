8c6faf889504dadf1c9f87a3bc084be77be78206
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-09-24 07:45:54 +0530

    Add "Bricks and building" - score 80

--- Hakerearth/September circuits '17/Bricks and building/Bricks and building.cpp
+++ Hakerearth/September circuits '17/Bricks and building/Bricks and building.cpp
@@ -1,36 +1,41 @@
 #include <stdio.h>
 #include <ios>
-
-#define MAX_BUILDINGS 100200
-int heights[MAX_BUILDINGS];
+#include <unordered_map>
 
 int main(int argc, char* argv[])
 {
 	std::ios::sync_with_stdio(false);
 
 	int n, h;
+	std::unordered_map<int, int> f;
+
 	scanf("%d", &n);
 	for (auto i = 0; i < n; ++i)
 	{
 		scanf("%d", &h);
-		heights[i] = h;
+		++f[h];
 	}
 
+	std::unordered_map<int, int> res;
 	int q, k;
 	scanf("%d", &q);
 	while (q--)
 	{
 		int c = 0;
 		scanf("%d", &k);
-		for (auto i = 0; i < n; ++i)
+		if (res.find(k) == res.end())
 		{
-			if (!(heights[i] % k))
+			for (const auto pair : f)
 			{
-				++c;
+				if (!(pair.first % k))
+				{
+					c += pair.second;
+				}
 			}
+			res[k] = c;
 		}
 
-		printf("%d\n", c);
+		printf("%d\n", res[k]);
 	}
 
 	return 0;

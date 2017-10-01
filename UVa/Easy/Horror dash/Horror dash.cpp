a7741e0c49f0a31c02c94684e885d6539d67e716
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-10-01 15:26:05 +0530

    Move "Horror dash" to separate project

---
+++ UVa/Easy/Horror dash/Horror dash.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ios>
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+
+	int T, c = 1;
+	scanf("%d", &T);
+	while (T--)
+	{
+		int N, max_speed, speed;
+		scanf("%d%d", &N, &max_speed);
+		for (int i = 0; i < N - 1; ++i)
+		{
+			scanf("%d", &speed);
+			if (speed > max_speed)
+			{
+				max_speed = speed;
+			}
+		}
+
+		printf("Case %d: %d\n", c++, max_speed);
+	}
+}

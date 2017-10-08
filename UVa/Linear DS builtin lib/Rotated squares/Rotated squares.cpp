16c03a122e5e98b5ce17d82f1c0d88b769e9e56c
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-10-08 12:43:09 +0530

    Add "Rotated squares"

--- UVa/Linear DS builtin lib/Rotated squares/Rotated squares.cpp
+++ UVa/Linear DS builtin lib/Rotated squares/Rotated squares.cpp
@@ -5,41 +5,43 @@
 using big_int = unsigned long long;
 inline void Swap(char *a, char *b)
 {
-	auto t = *a;
+	const auto t = *a;
 	*a = *b;
 	*b = t;
 }
 
-class Matrix
+class SqMatrix
 {
 public:
-	Matrix(big_int dim);
-	~Matrix();
+	SqMatrix(big_int dim);
+	~SqMatrix();
 
 	void PrintMatrix();
 	void Rotate90Clk();
+	big_int FindMatchF(const SqMatrix &n) const;
 
 private:
 	big_int dim;
 	std::vector<std::string> m;
+	bool MatchSubMatrix(const SqMatrix &n, big_int m_i, big_int m_j) const;
 };
 
-Matrix::Matrix(big_int dim)
+SqMatrix::SqMatrix(big_int dim)
 {
 	this->dim = dim;
 	m.reserve(dim);
 	for (auto i = 0; i < dim; i++)
 	{
 		m.push_back("");
-		std::getline(std::cin, m[i]);
+		getline(std::cin, m[i]);
 	}
 }
 
-Matrix::~Matrix()
+SqMatrix::~SqMatrix()
 {
 }
 
-void Matrix::PrintMatrix()
+void SqMatrix::PrintMatrix()
 {
 	for (auto i = 0; i < dim; i++)
 	{
@@ -47,26 +49,62 @@ void Matrix::PrintMatrix()
 	}
 }
 
-void Matrix::Rotate90Clk()
+void SqMatrix::Rotate90Clk()
 {
-	if (dim == 1)
-	{
-		return;
-	}
-
 	for (auto i = 0; i < dim >> 1; i++)
 	{
-		auto &a = m[i][i];
-		auto &b = m[i][dim - i - 1];
-		auto &c = m[dim - i - 1][dim - i - 1];
-		auto &d = m[dim - i - 1][i];
+		for (auto j = 0; j < (dim + 1) >> 1; j++)
+		{
+			auto &a = m[i][j];
+			auto &b = m[j][dim - i - 1];
+			auto &c = m[dim - i - 1][dim - j - 1];
+			auto &d = m[dim - j - 1][i];
 
-		auto t = a;
-		Swap(&b, &t);
-		Swap(&c, &t);
-		Swap(&d, &t);
-		Swap(&a, &t);
+			auto t = a;
+			Swap(&b, &t);
+			Swap(&c, &t);
+			Swap(&d, &t);
+			Swap(&a, &t);
+		}
 	}
+}
+
+big_int SqMatrix::FindMatchF(const SqMatrix& n) const
+{
+	big_int f = 0;
+	if (dim < n.dim)
+	{
+		return f;
+	}
+
+	for (auto i = 0; i <= dim - n.dim; ++i)
+	{
+		for (auto j = 0; j <= dim - n.dim; ++j)
+		{
+			if (MatchSubMatrix(n, i, j))
+			{
+				++f;
+			}
+		}
+	}
+
+	return f;
+}
+
+bool SqMatrix::MatchSubMatrix(const SqMatrix& n, big_int m_i, big_int m_j) const
+{
+	for (auto n_i = 0; n_i < n.dim; ++n_i)
+	{
+		for (auto n_j = 0; n_j < n.dim; ++n_j)
+		{
+			if (m[m_i + n_i][m_j + n_j] != n.m[n_i][n_j])
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
 }
 
 int main()
@@ -74,21 +112,21 @@ int main()
 	big_int N, n;
 	while (scanf("%llu %llu\n", &N, &n), N || n)
 	{
-		Matrix nm(N);
-		Matrix Nm(n);
-		nm.PrintMatrix();
-		std::cout << "After rotating" << std::endl;
+		SqMatrix Nm(N);
+		SqMatrix nm(n);
+
+		const auto r0 = Nm.FindMatchF(nm);
 		nm.Rotate90Clk();
-		nm.PrintMatrix();
-		std::cout << "After rotating" << std::endl;
+
+		const auto r90 = Nm.FindMatchF(nm);
 		nm.Rotate90Clk();
-		nm.PrintMatrix();
-		std::cout << "After rotating" << std::endl;
+
+		const auto r180 = Nm.FindMatchF(nm);
 		nm.Rotate90Clk();
-		nm.PrintMatrix();
-		std::cout << "After rotating" << std::endl;
-		nm.Rotate90Clk();
-		nm.PrintMatrix();
+
+		const auto r270 = Nm.FindMatchF(nm);
+
+		printf("%llu %llu %llu %llu\n", r0, r90, r180, r270);
 	}
 
 	return 0;

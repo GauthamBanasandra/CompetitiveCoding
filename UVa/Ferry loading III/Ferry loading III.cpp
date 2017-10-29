77f8c146a4ae29f77c1c797e81c82ce52322a965
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-10-29 12:37:01 +0530

    Add "Ferry loading III"

--- UVa/Ferry loading III/Ferry loading III.cpp
+++ UVa/Ferry loading III/Ferry loading III.cpp
@@ -5,37 +5,48 @@
 using big_int = unsigned long long;
 
 enum class river_bank { k_left, k_right };
+struct Car
+{
+	int id;
+	int arrival_time;
+	big_int unboard_time;
+};
+
+bool operator<(Car const &lhs, Car const &rhs) noexcept
+{
+	return rhs.id < lhs.id;
+}
 
 class RiverBank
 {
 public:
-	void add_car(const int time) { cars_.push(time); }
-	void remove_front_car();
-	bool has_cars()const { return !cars_.empty(); }
-	int front_car();
-	bool has_lesser_wait(const RiverBank* other);
+	void add_car (const Car &car) noexcept { cars_.push(car); }
+	void remove_front_car() noexcept;
+	bool has_cars() const noexcept { return !cars_.empty(); }
+	Car &front_car() noexcept;
+	bool has_lesser_wait(const RiverBank* other) noexcept;
 
 private:
-	std::queue<int> cars_;
+	std::queue<Car> cars_;
 };
 
-inline void RiverBank::remove_front_car()
+inline void RiverBank::remove_front_car() noexcept
 {
 	assert(has_cars());
 	cars_.pop();
 }
 
-int RiverBank::front_car()
+Car &RiverBank::front_car() noexcept
 {
 	assert(has_cars());
 	return cars_.front();
 }
 
-inline bool RiverBank::has_lesser_wait(const RiverBank *other)
+inline bool RiverBank::has_lesser_wait(const RiverBank *other) noexcept
 {
 	if (has_cars() && other->has_cars())
 	{
-		return front_car() <= other->cars_.front();
+		return front_car().arrival_time <= other->cars_.front().arrival_time;
 	}
 
 	if (has_cars() && !other->has_cars())
@@ -55,58 +66,60 @@ class Ferry
 {
 public:
 	Ferry(const int capacity, const int transport_time)
-		: clock(0), current_bank(river_bank::k_left), car_count_(0), capacity_(capacity),
+		: clock(0), current_bank(river_bank::k_left), capacity_(capacity),
 		transport_time_(transport_time)
 	{
 	}
 
-	void move_to_left();
-	void move_to_right();
-	bool is_vacant() const { return car_count_ < capacity_; }
-	void unload_cars();
-	void load_cars(RiverBank* rb);
-	bool is_empty() const { return car_count_ == 0; }
+	void move_to_left() noexcept;
+	void move_to_right() noexcept;
+	bool is_vacant() const noexcept { return cars_.size() < capacity_; }
+	void unload_cars(std::priority_queue<Car> &ferry_log) noexcept;
+	void load_cars(RiverBank* rb) noexcept;
+	bool is_empty() const noexcept { return cars_.empty(); }
+	void add_car(const Car &car) noexcept { cars_.push(car); }
 
 	big_int clock;
 	river_bank current_bank;
 
 private:
-	int car_count_;
+	std::queue<Car> cars_;
 	int capacity_;
 	int transport_time_;
 };
 
-inline void Ferry::move_to_left()
+inline void Ferry::move_to_left() noexcept
 {
 	clock += transport_time_;
 	current_bank = river_bank::k_left;
 }
 
-inline void Ferry::move_to_right()
+inline void Ferry::move_to_right() noexcept
 {
 	clock += transport_time_;
 	current_bank = river_bank::k_right;
 }
 
-void Ferry::unload_cars()
+void Ferry::unload_cars(std::priority_queue<Car>& ferry_log) noexcept
 {
-	while (car_count_-- > 0)
+	while (!is_empty())
 	{
-		printf("%llu\n", clock);
+		auto& car = cars_.front();
+		car.unboard_time = clock;
+		ferry_log.push(car);
+		cars_.pop();
 	}
-
-	car_count_ = 0;
 }
 
-void Ferry::load_cars(RiverBank *rb)
+void Ferry::load_cars(RiverBank *rb) noexcept
 {
-	while (is_vacant() && rb->has_cars() && rb->front_car() <= clock)
+	while (is_vacant() && rb->has_cars() && rb->front_car().arrival_time <= clock)
 	{
-		++car_count_;
+		add_car(rb->front_car());
 		rb->remove_front_car();
 	}
 
-	assert(car_count_ <= capacity_);
+	assert(cars_.size() <= capacity_);
 }
 
 class RiverFerry
@@ -119,19 +132,21 @@ public:
 	{
 	}
 
-	void begin_transport() const;
+	void begin_transport() noexcept;
+	void print_log() noexcept;
 
 private:
 	Ferry *ferry_;
 	RiverBank *left_bank_;
 	RiverBank *right_bank_;
+	std::priority_queue<Car> ferry_log_;
 };
 
-void RiverFerry::begin_transport() const
+void RiverFerry::begin_transport() noexcept
 {
 	while (left_bank_->has_cars() || right_bank_->has_cars() || !ferry_->is_empty())
 	{
-		ferry_->unload_cars();
+		ferry_->unload_cars(ferry_log_);
 		switch (ferry_->current_bank)
 		{
 		case river_bank::k_left:
@@ -145,12 +160,15 @@ void RiverFerry::begin_transport() const
 
 				if (left_bank_->has_lesser_wait(right_bank_))
 				{
-					ferry_->clock = left_bank_->front_car();
+					ferry_->clock = left_bank_->front_car().arrival_time;
 				}
 				else
 				{
-					ferry_->clock += right_bank_->front_car() - ferry_->clock;
-					assert(ferry_->clock >= 0);
+					if (right_bank_->front_car().arrival_time > ferry_->clock)
+					{
+						ferry_->clock += right_bank_->front_car().arrival_time - ferry_->clock;
+					}
+
 					ferry_->move_to_right();
 				}
 			}
@@ -171,12 +189,15 @@ void RiverFerry::begin_transport() const
 
 				if (right_bank_->has_lesser_wait(left_bank_))
 				{
-					ferry_->clock = right_bank_->front_car();
+					ferry_->clock = right_bank_->front_car().arrival_time;
 				}
 				else
 				{
-					ferry_->clock += left_bank_->front_car() - ferry_->clock;
-					assert(ferry_->clock >= 0);
+					if (left_bank_->front_car().arrival_time > ferry_->clock)
+					{
+						ferry_->clock += left_bank_->front_car().arrival_time - ferry_->clock;
+					}
+
 					ferry_->move_to_left();
 				}
 			}
@@ -186,6 +207,16 @@ void RiverFerry::begin_transport() const
 			}
 			break;
 		}
+	}
+}
+
+void RiverFerry::print_log() noexcept
+{
+	while (!ferry_log_.empty())
+	{
+		auto &entry = ferry_log_.top();
+		printf("%llu\n", entry.unboard_time);
+		ferry_log_.pop();
 	}
 }
 
@@ -207,16 +238,17 @@ int main(int argc, char* argv[])
 		Ferry ferry(n, t);
 		RiverBank left_bank;
 		RiverBank right_bank;
-		while (m--)
+		for (auto i = 0; i < m; ++i)
 		{
 			scanf("%d %s", &m_i, bank);
+			const Car car = { i, m_i };
 			switch (bank[0])
 			{
 			case 'l':
-				left_bank.add_car(m_i);
+				left_bank.add_car(car);
 				break;
 			case 'r':
-				right_bank.add_car(m_i);
+				right_bank.add_car(car);
 				break;
 			default:
 				assert(false);
@@ -225,6 +257,7 @@ int main(int argc, char* argv[])
 
 		RiverFerry river_ferry(&ferry, &left_bank, &right_bank);
 		river_ferry.begin_transport();
+		river_ferry.print_log();
 	}
 
 	return 0;

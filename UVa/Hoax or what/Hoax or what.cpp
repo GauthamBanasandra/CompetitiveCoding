43130c4490446308cf7bb882f7dd52aa7a42bba7
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-12-11 22:44:26 +0530

    Add "Hoax or what"

---
+++ UVa/Hoax or what/Hoax or what.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <set>
+
+int main(int argc, char* argv[])
+{
+	long int n, promotion_cost = 0, bill = 0, k = 0;
+	std::string line;
+	std::multiset<unsigned long long> urn;
+	while (std::cin >> n, n)
+	{
+		std::cin.ignore();
+		promotion_cost = 0;
+		urn.clear();
+		while (n--)
+		{
+			getline(std::cin, line);
+			std::istringstream tokenizer(line);
+			tokenizer >> k;
+			while (k-- > 0)
+			{
+				tokenizer >> bill;
+				urn.insert(bill);
+			}
+
+			const auto least_it = urn.begin();
+			const auto greatest_it = urn.rbegin();
+
+			promotion_cost += *greatest_it - *least_it;
+			urn.erase(prev(urn.end()));
+			urn.erase(least_it);
+		}
+
+		std::cout << promotion_cost << std::endl;
+	}
+
+	return 0;
+}

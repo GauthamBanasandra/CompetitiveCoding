b75093e7a3708f51132bcb6393a091f86f09a367
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-01-10 23:29:45 +0530

    Add "Integer sequence from"

---
+++ UVa/UVa/Iterative - linear scan/Integer sequence from/Integer sequence from.cpp
@@ -0,0 +1,62 @@
+//
+// Created by gauth on 10-01-2018.
+//
+
+#include <iostream>
+#include <sstream>
+#include <list>
+
+using ui = unsigned int;
+using ull = unsigned long long;
+
+ull pow(ull b, ull e) {
+  ull p = 1;
+  for (ull i = 0; i < e; ++i) {
+    p *= b;
+  }
+
+  return p;
+}
+
+int main() {
+  int c;
+  ui i, cf, d;
+  ull k, n, t;
+  std::string line;
+  std::list<std::pair<ui, ui>> p;
+
+  std::cin >> c;
+  while (c-- > 0) {
+    std::cin.ignore();
+    std::getline(std::cin, line);
+    std::istringstream tokenizer(line);
+
+    tokenizer >> i;
+    p.clear();
+    for (int j = 0; j <= i; ++j) {
+      tokenizer >> cf;
+      if (cf != 0) {
+        p.emplace_back(cf, j);
+      }
+    }
+
+    std::cin >> d >> k;
+    n = 0;
+    for (size_t r = 0; r < k; ++n) {
+      r += (n + 1) * d;
+    }
+
+    if (n == 0) {
+      n = 1;
+    }
+
+    t = 0;
+    for (const auto &pi : p) {
+      t += pi.first * pow(n, pi.second);
+    }
+
+    std::cout << t << std::endl;
+  }
+
+  return 0;
+}
---
+++ UVa/UVa/Iterative - linear scan/Integer sequence from/run.cmd
@@ -0,0 +1,1 @@
+cmake-build-debug\Integer_sequence_from.exe < "Iterative - linear scan\Integer sequence from\Integer sequence from1.txt" | clip

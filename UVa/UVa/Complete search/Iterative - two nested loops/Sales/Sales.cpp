5c7cf705de47e86531bf76e81e9a0c5187b4ca9b
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-01-16 00:06:23 +0530

    [UVa] Add "Sales"

---
+++ UVa/UVa/Complete search/Iterative - two nested loops/Sales/Sales.cpp
@@ -0,0 +1,43 @@
+//
+// Created by gauth on 15-01-2018.
+//
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using ull = unsigned long long;
+
+int main() {
+  ull t, s;
+  int n;
+  std::string line;
+  std::vector<int> sales;
+
+  std::cin >> t;
+  while (t--) {
+    std::cin >> n;
+    std::cin.ignore();
+    sales.clear();
+    sales.assign(n, 0);
+
+    std::getline(std::cin, line);
+    std::istringstream tokenizer(line);
+    for (int i = 0; i < n; ++i) {
+      tokenizer >> sales[i];
+    }
+
+    s = 0;
+    for (int i = 1; i < n; ++i) {
+      for (int j = i - 1; j >= 0; --j) {
+        if (sales[j] <= sales[i]) {
+          ++s;
+        }
+      }
+    }
+
+    std::cout << s << std::endl;
+  }
+
+  return 0;
+}
---
+++ UVa/UVa/Complete search/Iterative - two nested loops/Sales/run.cmd
@@ -0,0 +1,1 @@
+cmake-build-debug\Sales.exe < "Complete search\Iterative - two nested loops\Sales\Sales3.txt" | clip

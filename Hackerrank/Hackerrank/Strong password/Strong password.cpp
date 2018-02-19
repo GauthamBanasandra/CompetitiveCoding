5f93de50ec83dbd22c39a2bd6d44b75e8edb398f
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-02-19 22:26:43 +0530

    Migrate Hackerrank solution to CMake

--- Hackerrank/Hackerrank/Strong password/Strong password.cpp
+++ Hackerrank/Hackerrank/Strong password/Strong password.cpp
@@ -12,104 +12,83 @@
 
 char classifier[CLASSIFIER_LEN];
 
-void init_classifier()
-{
-	for (auto i = 0; i < CLASSIFIER_LEN; ++i)
-	{
-		classifier[i] = -1;
-	}
+void init_classifier() {
+  for (auto i = 0; i < CLASSIFIER_LEN; ++i) {
+    classifier[i] = -1;
+  }
 
-	const std::string digits = "0123456789";
-	const std::string lower_case = "abcdefghijklmnopqrstuvwxyz";
-	const std::string upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	const std::string special_char = "!@#$%^&*()-+";
+  const std::string digits = "0123456789";
+  const std::string lower_case = "abcdefghijklmnopqrstuvwxyz";
+  const std::string upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const std::string special_char = "!@#$%^&*()-+";
 
-	for (const auto &digit : digits)
-	{
-		classifier[digit] = DIGIT;
-	}
+  for (const auto &digit : digits) {
+    classifier[digit] = DIGIT;
+  }
 
-	for (const auto &ch : lower_case)
-	{
-		classifier[ch] = LOWER_CASE;
-	}
+  for (const auto &ch : lower_case) {
+    classifier[ch] = LOWER_CASE;
+  }
 
-	for (const auto &ch : upper_case)
-	{
-		classifier[ch] = UPPER_CASE;
-	}
+  for (const auto &ch : upper_case) {
+    classifier[ch] = UPPER_CASE;
+  }
 
-	for (const auto &ch : special_char)
-	{
-		classifier[ch] = SPECIAL_CHAR;
-	}
+  for (const auto &ch : special_char) {
+    classifier[ch] = SPECIAL_CHAR;
+  }
 }
 
-int get_missing_strength(const std::string &line)
-{
-	auto missing_strength = 0;
+int get_missing_strength(const std::string &line) {
+  auto missing_strength = 0;
 
-	std::bitset<4> criteria;
-	criteria.set();
-	for (auto const &c : line)
-	{
-		if (!criteria.to_ullong())
-		{
-			break;
-		}
+  std::bitset<4> criteria;
+  criteria.set();
+  for (auto const &c : line) {
+    if (!criteria.to_ullong()) {
+      break;
+    }
 
-		switch (classifier[c])
-		{
-		case DIGIT:
-			criteria.reset(DIGIT);
-			break;
+    switch (classifier[c]) {
+      case DIGIT:criteria.reset(DIGIT);
+        break;
 
-		case LOWER_CASE:
-			criteria.reset(LOWER_CASE);
-			break;
+      case LOWER_CASE:criteria.reset(LOWER_CASE);
+        break;
 
-		case UPPER_CASE:
-			criteria.reset(UPPER_CASE);
-			break;
+      case UPPER_CASE:criteria.reset(UPPER_CASE);
+        break;
 
-		case SPECIAL_CHAR:
-			criteria.reset(SPECIAL_CHAR);
-			break;
+      case SPECIAL_CHAR:criteria.reset(SPECIAL_CHAR);
+        break;
 
-		default:
-			break;
-		}
-	}
+      default:break;
+    }
+  }
 
-	for (const auto & c : criteria.to_string())
-	{
-		if (c == '1')
-		{
-			++missing_strength;
-		}
-	}
+  for (const auto &c : criteria.to_string()) {
+    if (c == '1') {
+      ++missing_strength;
+    }
+  }
 
-	return missing_strength;
+  return missing_strength;
 }
 
-int main(int argc, char* argv[])
-{
-	init_classifier();
+int main(int argc, char *argv[]) {
+  init_classifier();
 
-	int n;
-	std::string line;
-	std::cin >> n;
-	std::cin.ignore();
-	getline(std::cin, line);
-	const auto missing_strength = get_missing_strength(line);
-	if (n >= 6)
-	{
-		std::cout << missing_strength << '\n';
-	}
-	else
-	{
-		std::cout << std::max(6 - n, missing_strength) << '\n';
-	}
+  int n;
+  std::string line;
+  std::cin >> n;
+  std::cin.ignore();
+  getline(std::cin, line);
+  const auto missing_strength = get_missing_strength(line);
+  if (n >= 6) {
+    std::cout << missing_strength << '\n';
+  } else {
+    std::cout << std::max(6 - n, missing_strength) << '\n';
+  }
 
-	return 0;
+  return 0;
 }
--- Hackerrank/Hackerrank/The salesman/The salesman.cpp
+++ Hackerrank/Hackerrank/The salesman/The salesman.cpp
@@ -4,48 +4,40 @@
 #include <sstream>
 #include <math.h>
 
-int main(int argc, char* argv[])
-{
-	int t, n, loc;
-	long long time = 0;
-	std::string line;
-	std::priority_queue<int> street;
-	std::cin >> t;
-	while (t--)
-	{
-		std::cin >> n;
-		std::cin.ignore();
-		time = 0;
-		while (!street.empty())
-		{
-			street.pop();
-		}
+int main(int argc, char *argv[]) {
+  int t, n, loc;
+  long long time = 0;
+  std::string line;
+  std::priority_queue<int> street;
+  std::cin >> t;
+  while (t--) {
+    std::cin >> n;
+    std::cin.ignore();
+    time = 0;
+    while (!street.empty()) {
+      street.pop();
+    }
 
-		getline(std::cin, line);
-		std::istringstream tokenizer(line);
-		while (tokenizer >> loc)
-		{
-			street.push(loc);
-		}
+    getline(std::cin, line);
+    std::istringstream tokenizer(line);
+    while (tokenizer >> loc) {
+      street.push(loc);
+    }
 
-		if (street.size() == 1)
-		{
-			time = 0;
-		}
-		else
-		{
-			auto prev = street.top();
-			street.pop();
-			while (!street.empty())
-			{
-				time += abs(prev - street.top());
-				prev = street.top();
-				street.pop();
-			}
-		}
+    if (street.size() == 1) {
+      time = 0;
+    } else {
+      auto prev = street.top();
+      street.pop();
+      while (!street.empty()) {
+        time += abs(prev - street.top());
+        prev = street.top();
+        street.pop();
+      }
+    }
 
-		std::cout << time << std::endl;
-	}
+    std::cout << time << std::endl;
+  }
 
-	return 0;
+  return 0;
 }

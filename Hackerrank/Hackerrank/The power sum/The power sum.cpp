e87cb9e574d8539b0c1060f3a733c88cfaa64c55
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-02-21 22:05:26 +0530

    [Hackerrank] Add The power sum - TLE

--- Hackerrank/Hackerrank/The power sum/The power sum.cpp
+++ Hackerrank/Hackerrank/The power sum/The power sum.cpp
@@ -3,12 +3,9 @@
 //
 
 #include <iostream>
-#include <list>
 #include <vector>
 
 using ull = unsigned long long;
-
-void Print(const std::list<ull> &nums);
 
 ull pow(ull b, ull e) {
   ull p = 1;
@@ -19,19 +16,9 @@ ull pow(ull b, ull e) {
   return p;
 }
 
-ull add_all(const std::list<ull> &nums) {
-  ull sum = 0;
-  for (auto n : nums) {
-    sum += n;
-  }
-
-  return sum;
-}
-
 int main() {
   ull x, n, count = 0;
   std::vector<ull> nums;
-  std::list<ull> nums_subset;
 
   std::cin >> x >> n;
   for (ull i = 1; i <= x; ++i) {
@@ -46,19 +33,21 @@ int main() {
   auto ns = pow(2, nums.size());
   for (ull i = 0; i < ns; ++i) {
     auto i_ = i;
-    ull c = 0;
-    nums_subset.clear();
+    ull c = 0, sum = 0;
     while (i_) {
       if (i_ & 1) {
-        nums_subset.emplace_back(nums[c]);
+        sum += nums[c];
+      }
+
+      if (sum > x) {
+        break;
       }
 
       ++c;
       i_ >>= 1;
     }
 
-//    Print(nums_subset);
-    if (add_all(nums_subset) == x) {
+    if (sum == x) {
       ++count;
     }
   }

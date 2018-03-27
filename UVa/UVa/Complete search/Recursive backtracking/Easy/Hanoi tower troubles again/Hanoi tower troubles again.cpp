f4a57b2c05641f2315a34dcc0cd2925da4a3930d
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-03-27 22:14:15 +0530

    [UVa] Add "Hanoi tower troubles again"

--- UVa/UVa/Complete search/Recursive backtracking/Easy/Hanoi tower troubles again/Hanoi tower troubles again.cpp
+++ UVa/UVa/Complete search/Recursive backtracking/Easy/Hanoi tower troubles again/Hanoi tower troubles again.cpp
@@ -7,8 +7,6 @@
 #include <stack>
 #include <algorithm>
 #include <cmath>
-#include <unordered_set>
-#include <string>
 
 using ul = unsigned long;
 using ui = unsigned int;
@@ -16,10 +14,6 @@ using ui = unsigned int;
 inline bool IsPerfectSquare(int n) {
   double r = sqrt(n);
   return !(r - trunc(r));
-}
-
-inline std::string Hash(int a, int b) {
-  return a > b ? std::to_string(a) + " " + std::to_string(b) : std::to_string(b) + " " + std::to_string(a);
 }
 
 class HanoiTowers {
@@ -31,40 +25,24 @@ class HanoiTowers {
   int n_towers;
 
  private:
-  int GetPole(int v,
-              int i_pole,
-              const std::vector<std::stack<int>> &poles,
-              std::unordered_set<std::string> &poles_tried);
-  int Solve(int v,
-            std::vector<std::stack<int>> &poles,
-            std::unordered_set<std::string> &poles_tried);
+  int GetPole(int v, const std::vector<std::stack<int>> &poles);
+  void Solve(int v, int &max_balls, std::vector<std::stack<int>> &poles);
 };
 
-int HanoiTowers::Solve(int v,
-                       std::vector<std::stack<int>> &poles,
-                       std::unordered_set<std::string> &poles_tried) {
-  int max_balls = 0;
-  for (int i = 0; i < n_towers; ++i) {
-    auto p = GetPole(v, i, poles, poles_tried);
-    if (p != -1) {
-      poles[p].emplace(v);
-      max_balls = std::max(max_balls, Solve(v + 1, poles, poles_tried));
-      poles[p].pop();
-    }
+void HanoiTowers::Solve(int v, int &max_balls, std::vector<std::stack<int>> &poles) {
+  auto p = GetPole(v, poles);
+  if (p == -1) {
+    return;
   }
 
-  return std::max(v - 1, max_balls);
+  poles[p].emplace(v);
+  max_balls = std::max(max_balls, v);
+  Solve(v + 1, max_balls, poles);
 }
 
-int HanoiTowers::GetPole(int v,
-                         int i_pole,
-                         const std::vector<std::stack<int>> &poles,
-                         std::unordered_set<std::string> &poles_tried) {
-  for (int i = i_pole; i < n_towers; ++i) {
-    auto neighbouring_balls = Hash(v, poles[i].empty() ? -1 : poles[i].top());
-    if ((poles[i].empty() || IsPerfectSquare(poles[i].top() + v))
-        && (poles_tried.find(neighbouring_balls) == poles_tried.end())) {
-      poles_tried.insert(neighbouring_balls);
+int HanoiTowers::GetPole(int v, const std::vector<std::stack<int>> &poles) {
+  for (int i = 0; i < n_towers; ++i) {
+    if (poles[i].empty() || IsPerfectSquare(poles[i].top() + v)) {
       return i;
     }
   }
@@ -74,13 +52,18 @@ int HanoiTowers::GetPole(int v,
 
 int HanoiTowers::Solve() {
   std::vector<std::stack<int>> poles(static_cast<ui>(n_towers), std::stack<int>());
-  std::unordered_set<std::string> poles_tried;
-  return Solve(1, poles, poles_tried);
+  int max_balls = 0;
+  Solve(1, max_balls, poles);
+  return max_balls;
 }
 
 int main() {
-  for (int i = 1; i <= 50; ++i) {
-    std::cout << i << " : " << HanoiTowers(i).Solve() << std::endl;
+  int n, t;
+
+  std::cin >> t;
+  while (t-- > 0) {
+    std::cin >> n;
+    std::cout << HanoiTowers(n).Solve() << std::endl;
   }
 
   return 0;
---
+++ UVa/UVa/Complete search/Recursive backtracking/Easy/Hanoi tower troubles again/run.cmd
@@ -0,0 +1,1 @@
+cmake-build-debug\Hanoi_tower_troubles_again.exe < "Complete search/Recursive backtracking/Easy/Hanoi tower troubles again/Hanoi tower troubles again2.txt" | clip

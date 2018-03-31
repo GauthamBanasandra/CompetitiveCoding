413421d56141616b266a7abb7a79b9393aec5561
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-03-31 15:37:24 +0530

    [UVa] Move "Ad hoc/Time waster problems" under cmake

---
+++ UVa/UVa/Ad hoc/Time waster problems/All integer average/All integer average.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <math.h>
+#include <ios>
+
+using big_int = long long;
+
+big_int Gcd(big_int m, big_int n) {
+    big_int r;
+    while (n) {
+        r = m % n;
+        m = n;
+        n = r;
+    }
+
+    return m;
+}
+
+int CountDigits(big_int num) {
+    auto digits = 0;
+    while (num) {
+        ++digits;
+        num /= 10;
+    }
+
+    return digits;
+}
+
+int main(int argc, char *argv[]) {
+    std::ios::sync_with_stdio(false);
+
+    auto tc = 0;
+    big_int c;
+    while (scanf("%lld", &c), c) {
+        printf("Case %d:\n", ++tc);
+
+        big_int b = 0, _b;
+        for (auto i = 0; i < c; ++i) {
+            scanf("%lld", &_b);
+            b += _b;
+        }
+
+        const auto is_neg = b < 0;
+        b = abs(b);
+        if (!(b % c)) {
+            printf("%s%lld\n", is_neg ? "- " : "", b / c);
+        } else {
+            const auto gcd = Gcd(b, c);
+            b /= gcd;
+            c /= gcd;
+            if (b < c) {
+                const auto c_digits = CountDigits(c);
+                const auto padding = (is_neg ? 2 : 0) + c_digits;
+                printf("%*lld\n", padding, b);
+                if (is_neg) {
+                    printf("- ");
+                }
+
+                for (auto i = 0; i < c_digits; ++i) {
+                    printf("-");
+                }
+
+                printf("\n%*lld\n", padding, c);
+            } else if (b > c) {
+                const auto a = b / c;
+                b %= c;
+                const auto a_digits = CountDigits(a);
+                const auto c_digits = CountDigits(c);
+                const auto padding = (is_neg ? 2 : 0) + a_digits + c_digits;
+                printf("%*lld\n", padding, b);
+                printf("%s%lld", is_neg ? "- " : "", a);
+
+                for (auto i = 0; i < c_digits; ++i) {
+                    printf("-");
+                }
+
+                printf("\n%*lld\n", padding, c);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ UVa/UVa/Ad hoc/Time waster problems/Mobile casanova/Mobile casanova.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string>
+#include <ios>
+
+using big_int = unsigned long long;
+
+int DiffIdx(const std::string &m, const std::string &n) {
+    for (auto i = 0; i < m.length(); ++i) {
+        if (m[i] != n[i]) {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    std::ios::sync_with_stdio(false);
+
+    big_int n, _s, s, c, tc = 0;
+    while (scanf("%llu", &n), n) {
+        printf("Case %llu:\n", ++tc);
+        scanf("%llu", &s);
+        _s = s;
+        while (n > 0) {
+            auto is_con = false, c_init = false;
+            while (--n) {
+                scanf("%llu", &c);
+                c_init = true;
+                if (c - s == 1) {
+                    s = c;
+                    is_con = true;
+                    continue;
+                }
+
+                break;
+            }
+
+            if (is_con) {
+                const auto _s_str = std::to_string(_s);
+                const auto s_str = std::to_string(s);
+                printf("0%llu-%s\n", _s, s_str.substr(DiffIdx(s_str, _s_str)).c_str());
+            } else {
+                printf("0%llu\n", s);
+            }
+
+            if (c_init) {
+                _s = s = c;
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}

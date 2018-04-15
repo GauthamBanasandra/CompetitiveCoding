976fe433e3638eee210e1c2bb3eb6a9aa5dfc7cd
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-04-15 20:13:24 +0530

    [UVa] Move "DS with our own lib/Graph" under cmake

---
+++ UVa/UVa/DS with our own lib/Graph/Easy problem from rujia liu/Easy problem from rujia liu.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+
+int main(int argc, char *argv[]) {
+    int n, m, k;
+    long long v;
+    std::string line;
+    std::unordered_map<long long, std::vector<long long>> adj_list;
+
+    while (true) {
+        std::cin >> n >> m;
+        if (std::cin.eof()) {
+            break;
+        }
+
+        std::cin.ignore();
+        adj_list.clear();
+        getline(std::cin, line);
+        std::istringstream tokenizer(line);
+        for (long long i = 1; i <= n; ++i) {
+            tokenizer >> v;
+            adj_list[v].push_back(i);
+        }
+
+        while (m-- > 0) {
+            std::cin >> k >> v;
+            const auto find = adj_list.find(v);
+            if (find != adj_list.end() && k <= find->second.size()) {
+                std::cout << find->second[k - 1] << std::endl;
+            } else {
+                std::cout << 0 << std::endl;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ UVa/UVa/DS with our own lib/Graph/The forest for the trees/The forest for the trees.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <bitset>
+#include <string>
+
+int main(int argc, char *argv[]) {
+    unsigned long long n, edges = 0, acorns = 0, vertices = 0;
+    std::string line;
+    std::bitset<26> all_vertices;
+
+    std::cin >> n;
+    std::cin.ignore();
+    while (n--) {
+        vertices = acorns = edges = 0;
+        all_vertices.reset();
+        while (getline(std::cin, line), line[0] != '*') {
+            ++edges;
+            all_vertices.set(line[1] - 'A', true);
+            all_vertices.set(line[3] - 'A', true);
+        }
+
+        for (auto i = 'A' - 'A'; i <= 'Z' - 'A'; ++i) {
+            if (all_vertices[i]) {
+                ++vertices;
+            }
+        }
+
+        getline(std::cin, line);
+        for (const auto c : line) {
+            if (c != ',' && !all_vertices[c - 'A']) {
+                ++acorns;
+            }
+        }
+
+        std::cout << "There are " << vertices - edges << " tree(s) and " << acorns << " acorn(s)." << std::endl;
+    }
+
+    return 0;
+}

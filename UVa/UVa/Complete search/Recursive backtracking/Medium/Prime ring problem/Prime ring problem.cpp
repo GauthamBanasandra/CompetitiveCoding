609070d5e8f04609d20f20c06c26f369d7afe351
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-04-17 21:56:20 +0530

    [UVa] Add "Prime ring problem"

---
+++ UVa/UVa/Complete search/Recursive backtracking/Medium/Prime ring problem/Prime ring problem.cpp
@@ -0,0 +1,144 @@
+//
+// Created by gauth on 17-04-2018.
+//
+
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <cassert>
+#include <unordered_set>
+#include <iostream>
+
+struct RingUsageTracker {
+    RingUsageTracker() : ring(0) {}
+
+    void Use(int i) {
+        assert(i <= 16);
+        --i;
+        assert(i >= 0);
+        ring |= (1 << i);
+    }
+
+    void UndoUse(int i) {
+        assert(i <= 16);
+        --i;
+        assert(i >= 0);
+        ring &= ~(1 << i);
+    }
+
+    bool IsUsed(int i) {
+        assert(i <= 16);
+        --i;
+        assert(i >= 0);
+        return static_cast<bool>(ring & (1 << i));
+    }
+
+    int ring;
+};
+
+using Ring = std::vector<int>;
+
+struct RingArrangement {
+    void AddArrangement(const Ring &ring);
+
+    void PrintAndEmptyArrangements();
+
+    std::priority_queue<Ring, std::vector<Ring>, std::greater<Ring>> arrangements;
+};
+
+inline void RingArrangement::AddArrangement(const Ring &ring) {
+    assert(!ring.empty());
+    arrangements.emplace(ring);
+}
+
+void RingArrangement::PrintAndEmptyArrangements() {
+    while (!arrangements.empty()) {
+        const auto &arrangement = arrangements.top();
+        assert(!arrangement.empty());
+
+        std::ostringstream arrangement_stream;
+        arrangement_stream << arrangement[0];
+        for (std::size_t i = 1; i < arrangement.size(); ++i) {
+            arrangement_stream << " " << arrangement[i];
+        }
+
+        std::cout << arrangement_stream.str() << std::endl;
+        arrangements.pop();
+    }
+}
+
+class PrimeRing {
+public:
+    explicit PrimeRing(int n) : n(n) {}
+
+    RingArrangement Solve();
+
+private:
+    bool IsPrime(int number);
+
+    void FillNextRing(Ring &ring, int i_ring, RingUsageTracker &tracker, RingArrangement &arrangement);
+
+    int n;
+    static std::unordered_set<int> prime_numbers;
+};
+
+std::unordered_set<int> PrimeRing::prime_numbers{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+                                                 71, 73, 79, 83, 89, 97, 101};
+
+inline bool PrimeRing::IsPrime(int number) {
+    assert(number < 102);
+    return prime_numbers.find(number) != prime_numbers.end();
+}
+
+void
+PrimeRing::FillNextRing(Ring &ring, int i_ring, RingUsageTracker &tracker, RingArrangement &arrangement) {
+    if (i_ring >= n) {
+        if (!IsPrime(ring[0] + ring[i_ring - 1])) {
+            return;
+        }
+
+        arrangement.AddArrangement(ring);
+        return;
+    }
+
+    for (int i = 2; i <= n; ++i) {
+        if (tracker.IsUsed(i) || !IsPrime(ring[i_ring - 1] + i)) {
+            continue;
+        }
+
+        tracker.Use(i);
+        ring[i_ring] = i;
+
+        FillNextRing(ring, i_ring + 1, tracker, arrangement);
+
+        tracker.UndoUse(i);
+    }
+}
+
+RingArrangement PrimeRing::Solve() {
+    RingArrangement arrangement;
+    Ring ring(n, 1);
+    RingUsageTracker tracker;
+
+    tracker.Use(1);
+    FillNextRing(ring, 1, tracker, arrangement);
+    return arrangement;
+}
+
+int main() {
+    int t = 1;
+    std::string line;
+
+    while (std::getline(std::cin, line), !std::cin.eof()) {
+        if (t > 1) {
+            std::cout << std::endl;
+        }
+
+        printf("Case %d:\n", t++);
+        PrimeRing(atoi(line.c_str())).Solve().PrintAndEmptyArrangements();
+    }
+
+    return 0;
+}

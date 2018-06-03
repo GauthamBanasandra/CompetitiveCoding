af1ff7f1946583f59d479a3341c2a40e3ba93069
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-06-03 07:28:03 +0530

    [UVa] Move "Non linear DS builtin lib/C++ STL set" under cmake

---
+++ UVa/UVa/Non linear DS builtin lib/C++ STL set/CD/CD.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unordered_set>
+
+using big_int = unsigned long long;
+
+int main(int argc, char *argv[]) {
+    big_int n, m, id;
+    std::unordered_set<big_int> jack;
+    while (scanf("%llu%llu\n", &n, &m), n || m) {
+        jack.clear();
+        big_int count = 0;
+        while (n--) {
+            scanf("%llu\n", &id);
+            jack.insert(id);
+        }
+
+        while (m--) {
+            scanf("%llu\n", &id);
+            if (jack.find(id) != jack.end()) {
+                ++count;
+            }
+        }
+
+        printf("%llu\n", count);
+    }
+
+    return 0;
+}

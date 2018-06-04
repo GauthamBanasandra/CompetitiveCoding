dae809773d23930680a84d4bcdd464ef8c961d8a
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-06-04 22:40:30 +0530

    [UVa] Add "Popes"

---
+++ UVa/UVa/Divide and conquer/Binary search/Popes/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#cmake-build-debug/Popes < "Divide and conquer/Binary search/Popes/Popes.txt"
+#cmake-build-debug/Popes < "Divide and conquer/Binary search/Popes/Popes1.txt" | pbcopy
+cmake-build-debug/Popes < "Divide and conquer/Binary search/Popes/Popes2.txt" | pbcopy

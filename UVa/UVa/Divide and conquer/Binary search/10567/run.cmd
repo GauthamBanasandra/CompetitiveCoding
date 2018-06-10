5b77effb4eeecfc7070fc1ce44808320b545d79e
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-06-10 17:57:24 +0530

    [UVa] Add "Helping Fill Bates"

---
+++ UVa/UVa/Divide and conquer/Binary search/10567/run.cmd
@@ -0,0 +1,4 @@
+@echo off
+REM cmake-build-debug\Helping_fill_bates.exe < "Divide and conquer/Binary search/10567/Helping Fill Bates.txt"
+REM cmake-build-debug\Helping_fill_bates.exe < "Divide and conquer/Binary search/10567/Helping Fill Bates1.txt" | clip
+cmake-build-debug\Helping_fill_bates.exe < "Divide and conquer/Binary search/10567/Helping Fill Bates2.txt" | clip

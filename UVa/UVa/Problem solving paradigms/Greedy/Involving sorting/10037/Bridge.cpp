4d26ba20a900338c9eda6ee3b83e7edaeb2b4fc9
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-08-08 23:19:47 +0530

    [UVa] Add "Bridge" - WA

--- UVa/UVa/Problem solving paradigms/Greedy/Involving sorting/10037/Bridge.cpp
+++ UVa/UVa/Problem solving paradigms/Greedy/Involving sorting/10037/Bridge.cpp
@@ -7,6 +7,8 @@
 #include <vector>
 #include <cassert>
 #include <string>
+#include <sstream>
+#include <algorithm>
 
 enum class Protocol { kMoveSourceToDestination, kMoveDestinationToSource };
 enum class Command { kMoveFastestGroup, kMoveSlowestGroup, kMoveFastest };
@@ -14,12 +16,12 @@ enum class Command { kMoveFastestGroup, kMoveSlowestGroup, kMoveFastest };
 using Group = std::pair<int, int>;
 
 struct Event {
-  Event() : protocol(Protocol::kMoveSourceToDestination), p1_time(-1), p2_time(-1) {}
-  Event(Protocol protocol, int p1_time, int p2_time) : protocol(protocol), p1_time(p1_time), p2_time(p2_time) {}
+  Event() : protocol(Protocol::kMoveSourceToDestination), first(-1), second(-1) {}
+  Event(Protocol protocol, int first, int second) : protocol(protocol), first(first), second(second) {}
 
   Protocol protocol;
-  int p1_time;
-  int p2_time;
+  int first;
+  int second;
 };
 
 class Site {
@@ -44,6 +46,7 @@ class Site {
 
 Group Site::MoveFastestGroup(Site &destination) {
   assert(crossing_times_.size() >= 2);
+  assert(tag_ == "source");
 
   auto it = crossing_times_.begin();
   auto first = *it;
@@ -58,6 +61,7 @@ Group Site::MoveFastestGroup(Site &destination) {
 
 Group Site::MoveSlowestGroup(Site &destination) {
   assert(crossing_times_.size() >= 2);
+  assert(tag_ == "source");
 
   auto it = crossing_times_.rbegin();
   auto second = *it;
@@ -73,6 +77,7 @@ Group Site::MoveSlowestGroup(Site &destination) {
 
 int Site::MoveFastest(Site &source) {
   assert(!IsEmpty());
+  assert(tag_ == "destination");
 
   auto it = crossing_times_.begin();
   auto fastest = *it;
@@ -83,6 +88,8 @@ int Site::MoveFastest(Site &source) {
 
 Group Site::GetFastestGroup() const {
   assert(crossing_times_.size() >= 2);
+  assert(tag_ == "source");
+
   auto it = crossing_times_.begin();
   return {*it, *(++it)};
 }
@@ -95,36 +102,150 @@ void Site::Print() const {
   std::cout << std::endl;
 }
 
+class BridgeCrossing {
+ public:
+  BridgeCrossing(Site source, Site destination);
+  std::vector<Event> Start();
+  static void SpecialCase(int crossing_time);
+
+ private:
+  Command GetCommand(Protocol protocol) const;
+  Event Execute(Protocol protocol, Command command);
+  Protocol NextProtocol(Protocol protocol) const {
+    return protocol == Protocol::kMoveSourceToDestination ? Protocol::kMoveDestinationToSource
+                                                          : Protocol::kMoveSourceToDestination;
+  }
+
+  Group relays_;
+  Site source_;
+  Site destination_;
+};
+
+std::vector<Event> BridgeCrossing::Start() {
+  std::vector<Event> events;
+  auto protocol = Protocol::kMoveSourceToDestination;
+
+  while (!source_.IsEmpty()) {
+    auto command = GetCommand(protocol);
+    auto event = Execute(protocol, command);
+    events.emplace_back(event);
+    protocol = NextProtocol(protocol);
+  }
+  return events;
+}
+
+Command BridgeCrossing::GetCommand(Protocol protocol) const {
+  switch (protocol) {
+    case Protocol::kMoveSourceToDestination:
+      if (!destination_.Contains(relays_.first) && !destination_.Contains(relays_.second)) {
+        return Command::kMoveFastestGroup;
+      }
+
+      if ((!destination_.Contains(relays_.first) && destination_.Contains(relays_.second)) ||
+          (destination_.Contains(relays_.first) && destination_.Contains(relays_.second) &&
+              (relays_.first == relays_.second))) {
+        return Command::kMoveSlowestGroup;
+      }
+
+      assert(false);
+
+    case Protocol::kMoveDestinationToSource:return Command::kMoveFastest;
+  }
+}
+
+Event BridgeCrossing::Execute(Protocol protocol, Command command) {
+  switch (command) {
+    case Command::kMoveFastestGroup: {
+      auto group = source_.MoveFastestGroup(destination_);
+      return {protocol, group.first, group.second};
+    }
+
+    case Command::kMoveSlowestGroup: {
+      auto group = source_.MoveSlowestGroup(destination_);
+      return {protocol, group.first, group.second};
+    }
+
+    case Command::kMoveFastest: {
+      auto person = destination_.MoveFastest(source_);
+      return {protocol, person, -1};
+    }
+  }
+}
+
+BridgeCrossing::BridgeCrossing(Site source, Site destination)
+    : source_(std::move(source)), destination_(std::move(destination)) {
+  relays_ = source_.GetFastestGroup();
+}
+
+void BridgeCrossing::SpecialCase(int crossing_time) {
+  std::cout << crossing_time << std::endl << crossing_time << std::endl;
+}
+
+void PrintStrategy(const std::vector<Event> &events) {
+  std::ostringstream output;
+
+  long total_time = 0;
+  for (auto &event : events) {
+    switch (event.protocol) {
+      case Protocol::kMoveSourceToDestination: {
+        output << event.first << " " << event.second << std::endl;
+        total_time += std::max(event.first, event.second);
+        break;
+      }
+
+      case Protocol::kMoveDestinationToSource: {
+        output << event.first << std::endl;
+        total_time += event.first;
+        break;
+      }
+    }
+  }
+
+  std::cout << total_time << std::endl;
+  std::cout << output.str();
+}
+
 int main() {
-  std::multiset<int> crossing_times{4, 5, 1, 2, 1, 1, 5, 6};
+  int num_crossing_times = 0, crossing_time = 0, t = 0;
+  std::multiset<int> crossing_times{
+      1,
+      10,
+      10,
+      10,
+      100,
+      100,
+  };
+
   Site source(crossing_times, "source");
   Site destination("destination");
+  BridgeCrossing crossing(source, destination);
+  auto events = crossing.Start();
+  PrintStrategy(events);
 
-  std::cout << "Initial" << std::endl;
-  source.Print();
+  /*std::cin >> t;
+  for (int i = 1; i <= t; ++i) {
+    if (i > 1) {
+      std::cout << std::endl;
+    }
 
-  auto relays = source.GetFastestGroup();
-  std::cout << "Relays : " << relays.first << " " << relays.second << std::endl;
+    std::cin >> num_crossing_times;
+    if (num_crossing_times == 1) {
+      std::cin >> crossing_time;
+      BridgeCrossing::SpecialCase(crossing_time);
+      continue;
+    }
 
-  std::cout << "\nMoving fastest group from source to destination" << std::endl;
-  source.MoveFastestGroup(destination);
-  source.Print();
-  destination.Print();
+    crossing_times.clear();
+    for (int j = 0; j < num_crossing_times; ++j) {
+      std::cin >> crossing_time;
+      crossing_times.insert(crossing_time);
+    }
 
-  std::cout << "\nMoving fastest from destination to source" << std::endl;
-  destination.MoveFastest(source);
-  source.Print();
-  destination.Print();
-
-  std::cout << "\nMoving slowest group from source to destination" << std::endl;
-  source.MoveSlowestGroup(destination);
-  source.Print();
-  destination.Print();
-
-  std::cout << "\nMoving fastest from destination to source" << std::endl;
-  destination.MoveFastest(source);
-  source.Print();
-  destination.Print();
-
+    Site source(crossing_times, "source");
+    Site destination("destination");
+    BridgeCrossing crossing(source, destination);
+    auto events = crossing.Start();
+    PrintStrategy(events);
+  }*/
   return 0;
 }
---
+++ UVa/UVa/Problem solving paradigms/Greedy/Involving sorting/10037/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#cmake-build-debug/Bridge < "Problem solving paradigms/Greedy/Involving sorting/10037/Bridge.txt" | xclip -sel clip
+cmake-build-debug/Bridge < "Problem solving paradigms/Greedy/Involving sorting/10037/Bridge1.txt" | xclip -sel clip

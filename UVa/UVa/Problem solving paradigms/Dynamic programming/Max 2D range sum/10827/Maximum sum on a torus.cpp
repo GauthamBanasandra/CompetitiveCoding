cfb7234350b36f21a18804fac5297d8d48c8781d
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-10-25 23:20:32 +0530

    [UVa] Add "Maximum sum on a torus" - WA
    * Unable to understand the problem correctly.

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Max 2D range sum/10827/Maximum sum on a torus.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Max 2D range sum/10827/Maximum sum on a torus.cpp
@@ -5,39 +5,44 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cassert>
+#include <numeric>
 
 void Print(const std::vector<std::vector<int>> &matrix);
 
 using ll = long long;
 using Matrix = std::vector<std::vector<int>>;
 
-class WrappedMatrix {
+class ExtendedMatrix {
  public:
-  explicit WrappedMatrix(const Matrix &matrix);
+  explicit ExtendedMatrix(const Matrix &matrix);
+
+  ll MaxTorus();
 
  private:
   void ExtendHorizontal();
   void ExtendVertical();
   void ToCumulativeSumMatrix(Matrix &matrix);
   ll MaxTorus(Matrix &matrix) const;
+  ll TorusSum(const Matrix &matrix, std::size_t i, std::size_t j, std::size_t k, std::size_t l) const;
 
   Matrix horizontal_;
   Matrix vertical_;
 };
 
-WrappedMatrix::WrappedMatrix(const Matrix &matrix) : horizontal_(matrix), vertical_(matrix) {
+ExtendedMatrix::ExtendedMatrix(const Matrix &matrix) : horizontal_(matrix), vertical_(matrix) {
   ExtendHorizontal();
   ExtendVertical();
   ToCumulativeSumMatrix(horizontal_);
   ToCumulativeSumMatrix(vertical_);
 
-  std::cout << "Horizontal :" << std::endl;
+  /*std::cout << "Horizontal :" << std::endl;
   Print(horizontal_);
   std::cout << "\nVertical :" << std::endl;
-  Print(vertical_);
+  Print(vertical_);*/
 }
 
-void WrappedMatrix::ExtendHorizontal() {
+void ExtendedMatrix::ExtendHorizontal() {
   for (auto &row : horizontal_) {
     auto t_size = row.size();
     row.resize(t_size * 2);
@@ -47,7 +52,7 @@ void WrappedMatrix::ExtendHorizontal() {
   }
 }
 
-void WrappedMatrix::ExtendVertical() {
+void ExtendedMatrix::ExtendVertical() {
   auto t_size = vertical_.size();
   vertical_.resize(t_size * 2);
   for (int i = 0; i < t_size; ++i) {
@@ -55,7 +60,7 @@ void WrappedMatrix::ExtendVertical() {
   }
 }
 
-void WrappedMatrix::ToCumulativeSumMatrix(Matrix &matrix) {
+void ExtendedMatrix::ToCumulativeSumMatrix(Matrix &matrix) {
   for (std::size_t i = 0, row_len = matrix.size(); i < row_len; ++i) {
     for (std::size_t j = 0, column_len = matrix[i].size(); j < column_len; ++j) {
       if (i > 0) {
@@ -71,19 +76,48 @@ void WrappedMatrix::ToCumulativeSumMatrix(Matrix &matrix) {
   }
 }
 
-ll WrappedMatrix::MaxTorus(Matrix &matrix) const {
-  auto t_size = vertical_.size() / 2;
-  for (std::size_t i = 0, row_len = vertical_.size(); i < row_len; ++i) {
-    for (std::size_t j = 0, column_len = matrix[i].size(); j < column_len; ++j) {
-      for (auto k = i, torus_row_size = std::min(i + t_size, row_len); k < torus_row_size; ++k) {
+ll ExtendedMatrix::MaxTorus(Matrix &matrix) const {
+  assert(!vertical_.empty());
+  assert(!horizontal_.empty());
 
+  ll max_sum = std::numeric_limits<ll>::min();
+  auto actual_row_size = vertical_.size() / 2;
+  auto actual_column_size = horizontal_[0].size() / 2;
+
+  for (std::size_t i = 0, row_len = matrix.size(); i < row_len; ++i) {
+    for (std::size_t j = 0, column_len = matrix[i].size(); j < column_len; ++j) {
+      for (auto k = i, torus_row_size = std::min(i + actual_row_size, row_len); k < torus_row_size; ++k) {
+        for (auto l = j, torus_column_size = std::min(j + actual_column_size, column_len); l < torus_column_size; ++l) {
+          max_sum = std::max(max_sum, TorusSum(matrix, i, j, k, l));
+        }
       }
     }
   }
-  return 0;
+  return max_sum;
+}
+
+ll ExtendedMatrix::TorusSum(const Matrix &matrix, std::size_t i, std::size_t j, std::size_t k, std::size_t l) const {
+  auto sum = matrix[k][l];
+  if (i > 0) {
+    sum -= matrix[i - 1][l];
+  }
+  if (j > 0) {
+    sum -= matrix[k][j - 1];
+  }
+  if (i > 0 && j > 0) {
+    sum += matrix[i - 1][j - 1];
+  }
+  return sum;
+}
+
+ll ExtendedMatrix::MaxTorus() {
+  return std::max(MaxTorus(horizontal_), MaxTorus(vertical_));
 }
 
 int main() {
+  int t;
+  std::size_t n;
+
   Matrix matrix{
       /*{1, -1, 0, 0, -4},
       {2, 3, -2, -3, 2},
@@ -91,11 +125,32 @@ int main() {
       {3, -2, 1, -3, 2},
       {-3, 2, 4, 1, -4},*/
 
+      /*{1, 1, 1},
       {1, 1, 1},
-      {1, 1, 1},
-      {1, 1, 1}
+      {1, 1, 1}*/
+
+      /*{1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9},*/
+      {1, 0, 0, 1},
+      {0, -1, -1, 0},
+      {0, 4, -1, 0},
+      {1, 0, 0, 2},
   };
 
-  WrappedMatrix wrapped_matrix(matrix);
+  std::cin >> t;
+  while (t-- > 0) {
+    std::cin >> n;
+    matrix.resize(n);
+    for (std::size_t i = 0; i < n; ++i) {
+      matrix[i].resize(n);
+      for (std::size_t j = 0; j < n; ++j) {
+        std::cin >> matrix[i][j];
+      }
+    }
+
+    std::cout << ExtendedMatrix(matrix).MaxTorus() << std::endl;
+  }
+//  std::cout << ExtendedMatrix(matrix).MaxTorus() << std::endl;
   return 0;
 }
---
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Max 2D range sum/10827/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#cmake-build-debug/Maximum_sum_on_a_torus < "Problem solving paradigms/Dynamic programming/Max 2D range sum/10827/Maximum sum on a torus.txt"
+#cmake-build-debug/Maximum_sum_on_a_torus < "Problem solving paradigms/Dynamic programming/Max 2D range sum/10827/Maximum sum on a torus1.txt" | clip.exe
+#cmake-build-debug/Maximum_sum_on_a_torus < "Problem solving paradigms/Dynamic programming/Max 2D range sum/10827/Maximum sum on a torus2.txt" | clip.exe
+cmake-build-debug/Maximum_sum_on_a_torus < "Problem solving paradigms/Dynamic programming/Max 2D range sum/10827/Maximum sum on a torus3.txt" | clip.exe

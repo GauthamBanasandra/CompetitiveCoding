ec6e1fdbcae454dbfdb6b7a82ac17924fb66b723
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-03 16:16:17 +0530

    [Codechef] Add "Chef and difficult contests"

---
+++ Codechef/Compete/2018/November challenge/CHFTIRED/Chef and difficult contests.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ios>
+
+using ll = long long;
+
+int main(int argc, char* argv[])
+{
+	std::ios::sync_with_stdio(false);
+
+	int t;
+	ll a, b;
+
+	std::cin >> t;
+	while (t--)
+	{
+		std::cin >> a >> b;
+		std::cout << "YES" << std::endl;
+	}
+	return 0;
+}

bf52f862f0a9fc1d0d3631a4987df6cd5e7c95d5
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-03 22:56:41 +0530

    [Codechef] Add "Chef and ridges" - WIP

---
+++ Codechef/Compete/2018/November challenge/PRDRG/Chef and ridges.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>
+
+using ll = long long;
+
+struct Fraction
+{
+	Fraction() :numerator(0ll), denominator(0ll) {}
+	Fraction(const ll numerator, const ll denominator) :numerator(numerator), denominator(denominator) {}
+
+	ll numerator;
+	ll denominator;
+};
+
+Fraction GetRidgeWidth(const ll num_folds)
+{
+	return { 1, 1 << num_folds };
+}
+
+int main(int argc, char* argv[])
+{
+	int t;
+	ll n;
+	std::ostringstream output;
+
+	std::cin >> t;
+	for (auto i = 0; i < t; ++i)
+	{
+		std::cin >> n;
+		const auto fraction = GetRidgeWidth(n);
+		output << fraction.numerator << " " << fraction.denominator << " ";
+	}
+
+	auto output_str = output.str();
+	output_str.pop_back();
+	std::cout << output_str << std::endl;
+	return 0;
+}

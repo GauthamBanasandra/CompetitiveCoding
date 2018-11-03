404068affa295552d4301cd0c40d7938988a9e88
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-03 12:41:15 +0530

    [UVa] Add "History grading"

---
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/111/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#cmake-build-debug/History_grading < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/111/History grading.txt"
+#cmake-build-debug/History_grading < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/111/History grading1.txt" | pbcopy
+#cmake-build-debug/History_grading < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/111/History grading2.txt" | pbcopy
+cmake-build-debug/History_grading < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/111/History grading3.txt" | pbcopy

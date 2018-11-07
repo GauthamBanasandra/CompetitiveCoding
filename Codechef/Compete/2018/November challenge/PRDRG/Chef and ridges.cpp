81f810227b2d0d6e3f986711ce82957b4926f8fc
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-07 13:09:37 +0530

    [Codechef] Add "Chef and ridges"

--- Codechef/Compete/2018/November challenge/PRDRG/Chef and ridges.cpp
+++ Codechef/Compete/2018/November challenge/PRDRG/Chef and ridges.cpp
@@ -1,36 +1,134 @@
-// WA
-
 #include <iostream>
 #include <sstream>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+#include <ios>
 
-using ll = long long;
-
-struct Fraction
+struct ProperFraction
 {
-	Fraction() :numerator(0ll), denominator(0ll) {}
-	Fraction(const ll numerator, const ll denominator) :numerator(numerator), denominator(denominator) {}
+	ProperFraction() :numerator(0), denominator(1) {}
+	ProperFraction(const int numerator, const int denominator) :numerator(numerator), denominator(denominator) {}
 
-	ll numerator;
-	ll denominator;
+	ProperFraction operator-(const ProperFraction& other) const;
+	ProperFraction operator+(const ProperFraction& other) const;
+	void Halve() { denominator *= 2; }
+
+	int numerator;
+	int denominator;
 };
 
-Fraction GetRidgeWidth(const ll num_folds)
+ProperFraction ProperFraction::operator-(const ProperFraction& other) const
 {
-	return { 1, 1 << num_folds };
+	const auto max_d = std::max(denominator, other.denominator);
+	const auto min_d = std::min(denominator, other.denominator);
+	assert(max_d % min_d == 0);
+
+	const auto factor = max_d / min_d;
+
+	auto n = 0;
+	if (denominator < other.denominator)
+	{
+		n = numerator * factor - other.numerator;
+	}
+	else if (denominator > other.denominator)
+	{
+		n = numerator - other.numerator * factor;
+	}
+	else
+	{
+		n = numerator - other.numerator;
+	}
+	assert(n >= 0);
+	assert(n <= max_d);
+	return { n, max_d };
+}
+
+ProperFraction ProperFraction::operator+(const ProperFraction& other) const
+{
+	const auto max_d = std::max(denominator, other.denominator);
+	const auto min_d = std::min(denominator, other.denominator);
+	assert(max_d % min_d == 0);
+
+	const auto factor = max_d / min_d;
+
+	int n = 0;
+	if (denominator < other.denominator)
+	{
+		n = numerator * factor + other.numerator;
+	}
+	else if (denominator > other.denominator)
+	{
+		n = numerator + other.numerator * factor;
+	}
+	else
+	{
+		n = numerator + other.numerator;
+	}
+	assert(n < max_d);
+	return { n, max_d };
+}
+
+class RidgeTracker
+{
+public:
+	explicit RidgeTracker(std::size_t max_n);
+
+	ProperFraction GetRidgeAfter(std::size_t n) const;
+
+private:
+	std::size_t max_n_;
+	std::vector<std::pair<ProperFraction, ProperFraction>> states_;
+};
+
+RidgeTracker::RidgeTracker(const std::size_t max_n) :max_n_(max_n)
+{
+	states_.resize(max_n_ + 1);
+	ProperFraction l{ 0, 1 };
+	ProperFraction r{ 1, 1 };
+
+	for (std::size_t i = 0; i <= max_n_; ++i)
+	{
+		auto m = r - l;
+		m.Halve();
+
+		if (i & 1)
+		{
+			l = l + m;
+		}
+		else
+		{
+			r = l + m;
+		}
+
+		states_[i] = std::make_pair(l, r);
+	}
+}
+
+ProperFraction RidgeTracker::GetRidgeAfter(const std::size_t n) const
+{
+	assert(n <= max_n_);
+
+	if (n & 1)
+	{
+		return states_[n].second;
+	}
+	return states_[n].first;
 }
 
 int main(int argc, char* argv[])
 {
-	int t;
-	ll n;
+	std::ios::sync_with_stdio(false);
+	int t, n;
 	std::ostringstream output;
+	RidgeTracker tracker(25);
 
 	std::cin >> t;
 	for (auto i = 0; i < t; ++i)
 	{
 		std::cin >> n;
-		const auto fraction = GetRidgeWidth(n);
-		output << fraction.numerator << " " << fraction.denominator << " ";
+		const auto ridge = tracker.GetRidgeAfter(n);
+		output << ridge.numerator << " " << ridge.denominator << " ";
 	}
 
 	auto output_str = output.str();

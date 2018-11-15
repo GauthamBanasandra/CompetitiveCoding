a5f403d3e4847ea21184220192f212576a1e445c
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-15 23:16:30 +0530

    [UVa] Add problem ID to "Recursive backtracking/Medium"

---
+++ UVa/UVa/Problem solving paradigms/Complete search/Recursive backtracking/Medium/10503/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#cmake-build-debug/The_dominoes_solitaire < "Complete search/Recursive backtracking/Medium/10503/10503.txt"
+cmake-build-debug/The_dominoes_solitaire < "Complete search/Recursive backtracking/Medium/10503/The dominoes solitaire1.txt"
--- UVa/UVa/Problem solving paradigms/Complete search/Recursive backtracking/Medium/The dominoes solitaire/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#cmake-build-debug/The_dominoes_solitaire < "Complete search/Recursive backtracking/Medium/The dominoes solitaire/The dominoes solitaire.txt"
-cmake-build-debug/The_dominoes_solitaire < "Complete search/Recursive backtracking/Medium/The dominoes solitaire/The dominoes solitaire1.txt"

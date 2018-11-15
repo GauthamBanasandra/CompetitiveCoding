d87da993da80d3fa1eb0e3437a7fa2a697657c10
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-15 23:06:34 +0530

    [UVa] Add "What goes up"

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up.cpp
@@ -2,12 +2,11 @@
 // Created by Gautham on 14-11-2018.
 //
 
-// WA
-
 #include <algorithm>
 #include <vector>
 #include <iterator>
 #include <iostream>
+#include <ios>
 
 using ll = long long;
 
@@ -15,8 +14,44 @@ namespace ds {
 class seq {
  public:
   template<typename T>
+  static std::vector<T> longest_increasing_latest_elements(const std::vector<T> &numbers);
+
+  template<typename T>
   static std::vector<T> longest_increasing(const std::vector<T> &numbers);
 };
+
+template<typename T>
+std::vector<T> seq::longest_increasing_latest_elements(const std::vector<T> &numbers) {
+  const auto numbers_len = numbers.size();
+  std::vector<std::size_t> p(numbers_len);
+
+  std::vector<T> lis;
+  lis.reserve(numbers_len);
+
+  std::vector<std::size_t> m;
+  m.reserve(numbers_len + 1);
+  m.emplace_back(0);
+
+  for (std::size_t i = 0; i < numbers_len; ++i) {
+    auto find_it = std::lower_bound(m.begin() + 1, m.end(), numbers[i],
+                                    [&numbers](const std::size_t &i_m, const T &number) -> bool {
+                                      return numbers[i_m - 1] < number;
+                                    });
+    auto i_m = std::distance(m.begin(), find_it);
+    if (find_it == m.end()) {
+      m.emplace_back(i + 1);
+    } else {
+      *find_it = i + 1;
+    }
+    p[i] = m[i_m - 1];
+  }
+
+  for (auto i = m[m.size() - 1]; i > 0; i = p[i - 1]) {
+    lis.emplace_back(numbers[i - 1]);
+  }
+  std::reverse(lis.begin(), lis.end());
+  return lis;
+}
 
 template<typename T>
 std::vector<T> seq::longest_increasing(const std::vector<T> &numbers) {
@@ -48,15 +83,17 @@ std::vector<T> seq::longest_increasing(const std::vector<T> &numbers) {
 }
 
 int main() {
+  std::ios::sync_with_stdio(false);
+
   ll n;
-  std::vector<ll> numbers{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+  std::vector<ll> numbers;
   numbers.reserve(100000);
 
-  /*while (std::cin >> n, !std::cin.eof()) {
+  while (std::cin >> n, !std::cin.eof()) {
     numbers.emplace_back(n);
-  }*/
+  }
 
-  auto lis = ds::seq::longest_increasing(numbers);
+  auto lis = ds::seq::longest_increasing_latest_elements(numbers);
   std::cout << lis.size() << "\n-" << std::endl;
   for (auto number : lis) {
     std::cout << number << std::endl;
--- UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/run.sh
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 #cmake-build-debug/What_goes_up < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up.txt"
-cmake-build-debug/What_goes_up < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up1.txt" | clip.exe
+#cmake-build-debug/What_goes_up < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up1.txt" | clip.exe
+#cmake-build-debug/What_goes_up < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up2.txt" | clip.exe
+#cmake-build-debug/What_goes_up < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up3.txt" | clip.exe
+#cmake-build-debug/What_goes_up < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up4.txt" | clip.exe
+cmake-build-debug/What_goes_up < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/481/What goes up5.txt" | clip.exe

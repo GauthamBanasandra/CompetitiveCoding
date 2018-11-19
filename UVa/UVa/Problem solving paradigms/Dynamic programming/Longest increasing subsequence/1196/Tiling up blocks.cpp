76a7fd4ac5101a56455250a17815b5910479dbc6
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-19 17:05:18 +0530

    [UVa] Add "Tiling up blocks" - WIP

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/1196/Tiling up blocks.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/1196/Tiling up blocks.cpp
@@ -21,21 +21,38 @@ std::size_t count_longest(std::vector<Pair> &items) {
     return p.l < q.l;
   });
 
-  std::vector<Pair> lis;
-  lis.reserve(items.size());
+  const auto items_len = items.size();
+  std::vector<std::size_t> m;
+  m.reserve(items_len + 1);
+  m.emplace_back(0);
 
-  for (const auto &item : items) {
-    auto find_it = std::lower_bound(lis.begin(), lis.end(), item, [](const Pair &p, const Pair &q) -> bool {
-      return p.m <= q.m;
-    });
+  std::vector<std::size_t> predecessors(items_len);
 
-    if (find_it == lis.end()) {
-      lis.emplace_back(item);
+  for (std::size_t i = 0; i < items_len; ++i) {
+    auto find_it = std::upper_bound(m.begin() + 1,
+                                    m.end(),
+                                    i,
+                                    [&items](const std::size_t &i_m, const std::size_t &i_item) -> bool {
+                                      return items[i_m - 1].l < items[i_item].l;
+                                    });
+    auto i_m = std::distance(m.begin(), find_it);
+    if (find_it == m.end()) {
+      m.emplace_back(i + 1);
     } else {
-      *find_it = item;
+      *find_it = i + 1;
     }
+    predecessors[i] = m[i_m - 1];
   }
-  return lis.size();
+
+  std::vector<Pair> lis;
+  for (auto i = m[m.size() - 1]; i > 0; i = predecessors[i - 1]) {
+    lis.emplace_back(items[i - 1]);
+  }
+  std::reverse(lis.begin(), lis.end());
+  for (const auto &item : lis) {
+    std::cout << item.l << '\t' << item.m << std::endl;
+  }
+  return 0;
 }
 
 int main() {
@@ -46,9 +63,9 @@ int main() {
       {2, 3},
   };
 
-//  std::cout << count_longest(blocks) << std::endl;
+  std::cout << count_longest(blocks) << std::endl;
 
-  while (std::cin >> n, n) {
+  /*while (std::cin >> n, n) {
     blocks.resize(n);
     for (std::size_t i = 0; i < n; ++i) {
       std::cin >> blocks[i].l >> blocks[i].m;
@@ -56,6 +73,6 @@ int main() {
 
     std::cout << count_longest(blocks) << std::endl;
   }
-  std::cout << '*' << std::endl;
+  std::cout << '*' << std::endl;*/
   return 0;
 }

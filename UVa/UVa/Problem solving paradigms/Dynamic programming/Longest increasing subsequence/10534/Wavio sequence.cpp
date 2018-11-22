e83c703206333911d3bf3b5d4e33b6b6d15dc6b5
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-11-22 19:58:16 +0530

    [UVa] Add "Wavio sequence"

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence.cpp
@@ -6,23 +6,26 @@
 #include <algorithm>
 #include <iostream>
 #include <cassert>
-
-using ll = long long;
+#include <ios>
 
 void Print(const std::vector<std::size_t> &numbers, char separator = ' ');
 
 class Wavio {
  public:
-  explicit Wavio(const std::vector<ll> &seq) : seq_(seq) {}
+  explicit Wavio(const std::vector<int> &seq) : seq_(seq) {}
   std::size_t MaxWavioSize() const;
 
  private:
-  std::vector<std::size_t> GetPredecessors(const std::vector<ll> &seq) const;
+  std::vector<std::size_t> GetPredecessors(const std::vector<int> &seq) const;
+  std::vector<std::size_t> GetAintSeqLen(const std::vector<std::size_t> &predecessors) const;
+  std::size_t FiintSeqLen(const std::vector<std::size_t> &predecessors,
+                          std::size_t i,
+                          std::vector<std::size_t> &seq_len) const;
 
-  const std::vector<ll> &seq_;
+  const std::vector<int> &seq_;
 };
 
-std::vector<std::size_t> Wavio::GetPredecessors(const std::vector<ll> &seq) const {
+std::vector<std::size_t> Wavio::GetPredecessors(const std::vector<int> &seq) const {
   assert(!seq.empty());
   const auto len = seq.size();
 
@@ -36,7 +39,7 @@ std::vector<std::size_t> Wavio::GetPredecessors(const std::vector<ll> &seq) cons
     auto find_it = std::lower_bound(m.begin() + 1,
                                     m.end(),
                                     seq[i],
-                                    [&seq](const std::size_t &i_m, const ll &number) -> bool {
+                                    [&seq](const std::size_t &i_m, const int &number) -> bool {
                                       return seq[i_m - 1] < number;
                                     });
 
@@ -52,45 +55,63 @@ std::vector<std::size_t> Wavio::GetPredecessors(const std::vector<ll> &seq) cons
 }
 
 std::size_t Wavio::MaxWavioSize() const {
-  auto len = seq_.size();
-  for (int i = 1; i <= len; ++i) {
-    std::cout << i << '\t';
-  }
-  std::cout << std::endl << std::endl;
-
-  for (const auto &number : seq_) {
-    std::cout << number << '\t';
-  }
-  std::cout << std::endl;
+  std::vector<int> rev_seq(seq_.rbegin(), seq_.rend());
 
   auto predecessors = GetPredecessors(seq_);
-  for (const auto &number : predecessors) {
-    std::cout << number << '\t';
-  }
-  std::cout << std::endl << std::endl << std::endl;
+  auto rev_predecessors = GetPredecessors(rev_seq);
 
-  std::vector<ll> reverse_seq(seq_.rbegin(), seq_.rend());
-  for (int i = 1; i <= len; ++i) {
-    std::cout << i << '\t';
-  }
-  std::cout << std::endl << std::endl;
+  auto seq_len = GetAintSeqLen(predecessors);
+  auto rev_seq_len = GetAintSeqLen(rev_predecessors);
 
-  for (const auto &number : reverse_seq) {
-    std::cout << number << '\t';
-  }
-  std::cout << std::endl;
-  auto reverse_predecessors = GetPredecessors(reverse_seq);
-  for (const auto &number : reverse_predecessors) {
-    std::cout << number << '\t';
-  }
-  std::cout << std::endl << std::endl << std::endl;
+  std::reverse(rev_seq_len.begin(), rev_seq_len.end());
 
+  std::size_t max = 0;
+  for (std::size_t i = 0, len = seq_len.size(); i < len; ++i) {
+    max = std::max(max, std::min(seq_len[i], rev_seq_len[i]));
+  }
+  return 2 * max - 1;
+}
 
-  return 0;
+std::vector<std::size_t> Wavio::GetAintSeqLen(const std::vector<std::size_t> &predecessors) const {
+  auto len = predecessors.size();
+  std::vector<std::size_t> seq_len(len);
+
+  for (std::size_t i = 0; i < len; ++i) {
+    FiintSeqLen(predecessors, i, seq_len);
+  }
+  return seq_len;
+}
+
+std::size_t Wavio::FiintSeqLen(const std::vector<std::size_t> &predecessors,
+                               std::size_t i,
+                               std::vector<std::size_t> &seq_len) const {
+  if (predecessors[i] == 0) {
+    seq_len[i] = 1;
+    return 1;
+  }
+
+  if (seq_len[i] != 0) {
+    return seq_len[i];
+  }
+
+  auto len = FiintSeqLen(predecessors, predecessors[i] - 1, seq_len) + 1;
+  seq_len[i] = len;
+  return len;
 }
 
 int main() {
-  std::vector<ll> numbers{1, 2, 3, 2/*, 1, 2, 3, 4, 3, 2, 1, 5, 4, 1, 2, 3, 2, 2, 1*/};
-  Wavio(numbers).MaxWavioSize();
+  std::ios::sync_with_stdio(false);
+
+  std::size_t len;
+  std::vector<int> numbers{1, 2, 3, 2, 1, 2, 3, 4, 3, 2, 1, 5, 4, 1, 2, 3, 2, 2, 1};
+
+  while (std::cin >> len, !std::cin.eof()) {
+    numbers.resize(len);
+    for (std::size_t i = 0; i < len; ++i) {
+      std::cin >> numbers[i];
+    }
+
+    std::cout << Wavio(numbers).MaxWavioSize() << std::endl;
+  }
   return 0;
 }
---
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#cmake-build-debug/Wavio_sequence < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence.txt"
+#cmake-build-debug/Wavio_sequence < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence1.txt" > "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence 1.out.txt"
+#cmake-build-debug/Wavio_sequence < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence2.txt" > "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence 2.out.txt"
+#cmake-build-debug/Wavio_sequence < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence3.txt" > "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence 3.out.txt"
+cmake-build-debug/Wavio_sequence < "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence4.txt" > "Problem solving paradigms/Dynamic programming/Longest increasing subsequence/10534/Wavio sequence 4.out.txt"

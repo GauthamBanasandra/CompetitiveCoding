675ea2d9ec2725959d8a1811a14f98908ec09025
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-12-06 22:07:23 +0530

    [UVa] Add "Ferry loading" - WIP

--- UVa/UVa/Problem solving paradigms/Dynamic programming/0-1 Knapsack/10261/Ferry loading.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/0-1 Knapsack/10261/Ferry loading.cpp
@@ -1,3 +1,5 @@
+#include <utility>
+
 //
 // Created by Gautham on 06-12-2018.
 //
@@ -5,34 +7,48 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using ll = long long;
+
+struct FerryInfo {
+  FerryInfo(ll count, std::vector<std::string> directions) : count(count), directions(std::move(directions)) {}
+
+  ll count;
+  std::vector<std::string> directions;
+};
 
 class Ferry {
  public:
   Ferry(ll port_len, ll starboard_len, const std::vector<ll> &cars);
-  ll Direct();
+  FerryInfo Direct();
 
  private:
-  ll Direct(std::size_t i, ll port_len, ll starboard_len);
+  ll Direct(std::size_t i, ll port_len, ll starboard_len, std::vector<std::string> &directions);
 
   const ll port_len_;
   const ll starboard_len_;
   const std::vector<ll> &cars_;
-  std::vector<ll> memo_;
+  std::vector<std::vector<std::vector<ll>>> memo_;
 };
 
 Ferry::Ferry(const ll port_len, const ll starboard_len, const std::vector<ll> &cars)
     : port_len_(port_len), starboard_len_(starboard_len), cars_(cars) {
-  memo_.resize(cars_.size(), -1);
+  memo_.resize(cars_.size());
+  for (auto &table : memo_) {
+    table.resize(static_cast<std::size_t>(port_len) + 1);
+    for (auto &row : table) {
+      row.resize(static_cast<std::size_t>(starboard_len_) + 1, -1);
+    }
+  }
 }
 
-ll Ferry::Direct(std::size_t i, ll port_len, ll starboard_len) {
+ll Ferry::Direct(std::size_t i, ll port_len, ll starboard_len, std::vector<std::string> &directions) {
   if (i >= cars_.size()) {
     return 0;
   }
 
-  auto &memo = memo_[i];
+  auto &memo = memo_[i][port_len][starboard_len];
   if (memo != -1) {
     return memo;
   }
@@ -42,36 +58,47 @@ ll Ferry::Direct(std::size_t i, ll port_len, ll starboard_len) {
   }
 
   if (cars_[i] <= port_len && cars_[i] > starboard_len) {
-    return Direct(i + 1, port_len - cars_[i], starboard_len) + 1;
+    directions[i] = "port";
+    return Direct(i + 1, port_len - cars_[i], starboard_len, directions) + 1;
   }
 
   if (cars_[i] > port_len && cars_[i] <= starboard_len) {
-    return Direct(i + 1, port_len, starboard_len - cars_[i]) + 1;
+    directions[i] = "starboard";
+    return Direct(i + 1, port_len, starboard_len - cars_[i], directions) + 1;
   }
 
-  auto l = Direct(i + 1, port_len - cars_[i], starboard_len);
-  auto r = Direct(i + 1, port_len, starboard_len - cars_[i]);
-  memo = std::max(l, r) + 1;
+  auto l = Direct(i + 1, port_len - cars_[i], starboard_len, directions) + 1;
+  auto r = Direct(i + 1, port_len, starboard_len - cars_[i], directions) + 1;
+  if (l >= r) {
+    directions[i] = "port";
+    memo = l;
+  } else {
+    directions[i] = "starboard";
+    memo = r;
+  }
   return memo;
 }
 
-ll Ferry::Direct() {
-  return Direct(0, port_len_, starboard_len_);
+FerryInfo Ferry::Direct() {
+  std::vector<std::string> directions(cars_.size());
+  auto num_loaded = Direct(0, port_len_, starboard_len_, directions);
+  directions.resize(static_cast<std::size_t>(num_loaded));
+  return {num_loaded, directions};
 }
 
 int main() {
 //  ll ferry_len = 98;
 //  ll ferry_len = 20;
-  ll ferry_len = 50;
-  ll port_len = ferry_len * 100, starboard_len = ferry_len * 100;
+  std::size_t t;
+  ll ferry_len = 15, car_len = 0;
   std::vector<ll> cars{
-      2500,
+      /*2500,
       3000,
       1000,
       1000,
       1500,
       700,
-      800
+      800*/
 
       /*3551,
       8043,
@@ -92,8 +119,42 @@ int main() {
       188,
       311,
       112*/
+
+      100,
+      100,
+      200,
+      200,
+      300,
+      300,
+      400,
+      400,
+      500,
+      500
   };
 
-  std::cout << Ferry(port_len, starboard_len, cars).Direct() << std::endl;
+  /*auto info = Ferry(ferry_len * 100, ferry_len * 100, cars).Direct();
+  std::cout << info.count << std::endl;
+  for (const auto &direction : info.directions) {
+    std::cout << direction << std::endl;
+  }*/
+
+  std::cin >> t;
+  for (std::size_t i = 0; i < t; ++i) {
+    std::cin >> ferry_len;
+    cars.clear();
+    while (std::cin >> car_len, car_len) {
+      cars.emplace_back(car_len);
+    }
+
+    if (i > 0) {
+      std::cout << std::endl;
+    }
+
+    auto info = Ferry(ferry_len * 100, ferry_len * 100, cars).Direct();
+    std::cout << info.count << std::endl;
+    for (const auto &direction : info.directions) {
+      std::cout << direction << std::endl;
+    }
+  }
   return 0;
 }
---
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/0-1 Knapsack/10261/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#cmake-build-debug/Ferry_loading < "Problem solving paradigms/Dynamic programming/0-1 Knapsack/10261/Ferry loading.txt"
+cmake-build-debug/Ferry_loading < "Problem solving paradigms/Dynamic programming/0-1 Knapsack/10261/Ferry loading1.txt" | clip.exe

632e59e9fc081e2fb5767003b843617734916bc1
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-12-08 16:01:25 +0530

    [UVa] Add "Ferry loading"
    * This solution is quite inefficient

--- UVa/UVa/Problem solving paradigms/Dynamic programming/0-1 Knapsack/10261/Ferry loading.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/0-1 Knapsack/10261/Ferry loading.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <string>
 #include <unordered_map>
+#include <cassert>
 
 using ll = long long;
 
@@ -23,20 +24,24 @@ class Ferry {
   FerryInfo Direct();
 
  private:
-  ll Direct(std::size_t i, ll port_len, ll starboard_len, std::vector<std::string> &directions);
+  ll Direct(std::size_t i, ll port_len, ll starboard_len);
   bool IsExists(std::size_t i, ll port_len, ll starboard_len);
+  std::vector<std::string> GetDirections(std::size_t n);
 
   const ll port_len_;
   const ll starboard_len_;
   const std::vector<ll> &cars_;
   std::unordered_map<std::size_t, std::unordered_map<ll, std::unordered_map<ll, ll>>> memo_;
+  // Use unordered map instead of vector
+  // It's relatively faster but could essentially time out as we would still compute the same number of states
+  std::unordered_map<std::size_t, std::unordered_map<ll, std::unordered_map<ll, std::string>>> dir_memo_;
 };
 
 Ferry::Ferry(const ll port_len, const ll starboard_len, const std::vector<ll> &cars)
     : port_len_(port_len), starboard_len_(starboard_len), cars_(cars) {
 }
 
-ll Ferry::Direct(std::size_t i, ll port_len, ll starboard_len, std::vector<std::string> &directions) {
+ll Ferry::Direct(std::size_t i, ll port_len, ll starboard_len) {
   if (i >= cars_.size()) {
     return 0;
   }
@@ -50,32 +55,30 @@ ll Ferry::Direct(std::size_t i, ll port_len, ll starboard_len, std::vector<std::
   }
 
   if (cars_[i] <= port_len && cars_[i] > starboard_len) {
-    directions[i] = "port";
-    return Direct(i + 1, port_len - cars_[i], starboard_len, directions) + 1;
+    dir_memo_[i][port_len][starboard_len] = "port";
+    return Direct(i + 1, port_len - cars_[i], starboard_len) + 1;
   }
 
   if (cars_[i] > port_len && cars_[i] <= starboard_len) {
-    directions[i] = "starboard";
-    return Direct(i + 1, port_len, starboard_len - cars_[i], directions) + 1;
+    dir_memo_[i][port_len][starboard_len] = "starboard";
+    return Direct(i + 1, port_len, starboard_len - cars_[i]) + 1;
   }
 
-  auto l = Direct(i + 1, port_len - cars_[i], starboard_len, directions) + 1;
-  auto r = Direct(i + 1, port_len, starboard_len - cars_[i], directions) + 1;
+  auto l = Direct(i + 1, port_len - cars_[i], starboard_len) + 1;
+  auto r = Direct(i + 1, port_len, starboard_len - cars_[i]) + 1;
   if (l >= r) {
-    directions[i] = "port";
+    dir_memo_[i][port_len][starboard_len] = "port";
     memo_[i][port_len][starboard_len] = l;
   } else {
-    directions[i] = "starboard";
     memo_[i][port_len][starboard_len] = r;
+    dir_memo_[i][port_len][starboard_len] = "starboard";
   }
   return memo_[i][port_len][starboard_len];
 }
 
 FerryInfo Ferry::Direct() {
-  std::vector<std::string> directions(cars_.size());
-  auto num_loaded = Direct(0, port_len_, starboard_len_, directions);
-  directions.resize(static_cast<std::size_t>(num_loaded));
-  return {num_loaded, directions};
+  auto num_loaded = Direct(0, port_len_, starboard_len_);
+  return {num_loaded, GetDirections(static_cast<std::size_t>(num_loaded))};
 }
 
 bool Ferry::IsExists(std::size_t i, ll port_len, ll starboard_len) {
@@ -99,14 +102,33 @@ bool Ferry::IsExists(std::size_t i, ll port_len, ll starboard_len) {
   return true;
 }
 
+std::vector<std::string> Ferry::GetDirections(std::size_t n) {
+  std::vector<std::string> directions(n);
+  auto port_len = port_len_;
+  auto starboard_len = starboard_len_;
+
+  for (std::size_t i = 0; i < n; ++i) {
+    auto direction = dir_memo_[i][port_len][starboard_len];
+    directions[i] = direction;
+    if (direction == "port") {
+      port_len -= cars_[i];
+    } else if (direction == "starboard") {
+      starboard_len -= cars_[i];
+    } else {
+      assert(false);
+    }
+  }
+  return directions;
+}
+
 int main() {
 //  ll ferry_len = 98;
 //  ll ferry_len = 20;
 //  ll ferry_len = 20;
   std::size_t t;
-  ll ferry_len = 15, car_len = 0;
+  ll ferry_len = 20, car_len = 0;
   std::vector<ll> cars{
-      /*2500,
+     /* 2500,
       3000,
       1000,
       1000,
@@ -120,7 +142,7 @@ int main() {
       1798,
       1190*/
 
-      /*101,
+      101,
       207,
       155,
       143,
@@ -132,9 +154,9 @@ int main() {
       201,
       188,
       311,
-      112*/
+      112
 
-      100,
+      /*100,
       100,
       200,
       200,
@@ -143,7 +165,7 @@ int main() {
       400,
       400,
       500,
-      500
+      500*/
   };
 
   /*auto info = Ferry(ferry_len * 100, ferry_len * 100, cars).Direct();

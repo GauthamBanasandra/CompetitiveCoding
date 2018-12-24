821cf8a12ac5c3e16ec126e2a66eeb42664c260e
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-12-24 13:11:49 +0530

    [UVa] Add "Making change"
    * This solution is wrong, even though it passed, guessing it has a
      weak test case.

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Coin change/166/run.sh
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Coin change/166/run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 #cmake-build-debug/Making_change < "Problem solving paradigms/Dynamic programming/Coin change/166/Making change.txt"
-cmake-build-debug/Making_change < "Problem solving paradigms/Dynamic programming/Coin change/166/Making change1.txt" | clip.exe
+#cmake-build-debug/Making_change < "Problem solving paradigms/Dynamic programming/Coin change/166/Making change1.txt" | clip.exe
+#cmake-build-debug/Making_change < "Problem solving paradigms/Dynamic programming/Coin change/166/Making change2.txt" | clip.exe
+#cmake-build-debug/Making_change < "Problem solving paradigms/Dynamic programming/Coin change/166/Making change3.txt" | clip.exe
+cmake-build-debug/Making_change < "Problem solving paradigms/Dynamic programming/Coin change/166/Making change4.txt" | clip.exe

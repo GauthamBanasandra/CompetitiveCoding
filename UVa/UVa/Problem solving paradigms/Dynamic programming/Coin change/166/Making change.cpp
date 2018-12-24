61aa4abcf571f3d27631ed4b181b9378b5910cc8
GauthamBanasandra <gautham.bangalore@gmail.com>
2018-12-24 13:11:49 +0530

    [UVa] Add "Making change"
    * Much more improvements needed.

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Coin change/166/Making change.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Coin change/166/Making change.cpp
@@ -11,25 +11,44 @@
 
 const auto infinity = std::numeric_limits<int>::max();
 
-struct Denomination {
-  Denomination(int value, long count) : value(value), count(count) {}
-
-  int value;
-  long count;
-};
-
-enum Denominations {
+enum CoinType {
   k5,
   k10,
   k20,
   k50,
   k100,
-  k200
+  k200,
+  Count
 };
 
-class ChangeMaker {
+struct Denomination {
+  Denomination(CoinType type, long count);
+
+  CoinType type;
+  int value;
+  std::size_t count;
+};
+
+Denomination::Denomination(CoinType type, long count) : type(type), count(count) {
+  switch (type) {
+    case k5: value = 5;
+      break;
+    case k10:value = 10;
+      break;
+    case k20:value = 20;
+      break;
+    case k50:value = 50;
+      break;
+    case k100:value = 100;
+      break;
+    case k200:value = 200;
+      break;
+  }
+}
+
+class ShopKeeper {
  public:
-  explicit ChangeMaker(std::size_t max_value, std::vector<Denomination> denominations);
+  explicit ShopKeeper(std::size_t max_value);
 
   int Count(int change);
 
@@ -40,12 +59,12 @@ class ChangeMaker {
   std::vector<int> memo_;
 };
 
-ChangeMaker::ChangeMaker(std::size_t max_value, std::vector<Denomination> denominations) : denominations_(std::move(
-    denominations)) {
+ShopKeeper::ShopKeeper(std::size_t max_value) : denominations_({{k5, infinity}, {k10, infinity}, {k20, infinity},
+                                                                {k50, infinity}, {k100, infinity}, {k200, infinity}}) {
   memo_.resize(max_value + 1, -1);
 }
 
-int ChangeMaker::Count(int change) {
+int ShopKeeper::Count(int change) {
   if (change < 0) {
     return infinity;
   }
@@ -61,14 +80,7 @@ int ChangeMaker::Count(int change) {
 
   auto min_change = infinity;
   for (auto &denomination : denominations_) {
-    if (denomination.count == 0) {
-      continue;
-    }
-
-    --denomination.count;
-    auto b = Count(change - denomination.value);
-    min_change = std::min(min_change, b);
-    ++denomination.count;
+    min_change = std::min(min_change, Count(change - denomination.value));
   }
   return memo = min_change == infinity ? infinity : 1 + min_change;
 }
@@ -101,40 +113,80 @@ int CoinIterator::Next(int offset) {
   return denomination.value + offset;
 }
 
-class Customer : public ChangeMaker {
+class Customer {
  public:
-  Customer(std::size_t max_value, std::vector<Denomination> &denominations);
+  explicit Customer(std::vector<Denomination> &denominations);
 
   CoinIterator GetCoinIterator() const { return {denominations_}; }
   int GetBudget() const { return budget_; }
 
+  int Count(int change);
+
  private:
+  int Count(size_t i_denomination, std::size_t i_coin, int change);
+
   int budget_;
+  std::vector<Denomination> denominations_;
+  std::vector<std::vector<std::vector<int>>> memo_;
 };
 
-Customer::Customer(std::size_t max_value, std::vector<Denomination> &denominations) : ChangeMaker(max_value,
-                                                                                                  denominations),
-                                                                                      budget_(0) {
+Customer::Customer(std::vector<Denomination> &denominations)
+    : budget_(0), denominations_(denominations) {
   for (const auto &denomination : denominations_) {
     budget_ += denomination.value * denomination.count;
   }
+
+  memo_.resize(denominations_.size());
+  std::vector<int> row(budget_ + 1, -1);
+  for (std::size_t i = 0, len = denominations_.size(); i < len; ++i) {
+    memo_[i].resize(static_cast<std::size_t>(denominations_[i].count), row);
+  }
+}
+
+int Customer::Count(int change) {
+  return Count(0, denominations_[0].count, change);
+}
+
+int Customer::Count(std::size_t i_denomination, std::size_t i_coin, int change) {
+  if (change < 0 || i_denomination >= denominations_.size()) {
+    return infinity;
+  }
+  if (change == 0) {
+    return 0;
+  }
+
+  if (i_coin == 0) {
+    return Count(i_denomination + 1,
+                 i_denomination + 1 >= denominations_.size() ? infinity : denominations_[i_denomination + 1].count,
+                 change);
+  }
+
+  auto &memo = memo_[i_denomination][i_coin - 1][change];
+  if (memo != -1) {
+    return memo;
+  }
+
+  auto min_change = infinity;
+  for (std::size_t i = 0, len = denominations_.size(); i < len; ++i) {
+    auto &denomination = denominations_[i];
+    if (denomination.count == 0) {
+      continue;
+    }
+    --denomination.count;
+    min_change = std::min(min_change, Count(i, denomination.count, change - denomination.value));
+    ++denomination.count;
+  }
+  return memo = min_change == infinity ? infinity : 1 + min_change;
 }
 
 class Shop {
  public:
-  explicit Shop(std::size_t max_value) : shop_keeper_(max_value, {
-      {5, infinity},
-      {10, infinity},
-      {20, infinity},
-      {50, infinity},
-      {100, infinity},
-      {200, infinity}
-  }) {}
+  explicit Shop(std::size_t max_value) : shop_keeper_(max_value) {}
 
   int Buy(Customer &customer, int value);
 
  private:
-  ChangeMaker shop_keeper_;
+  ShopKeeper shop_keeper_;
 };
 
 int Shop::Buy(Customer &customer, int value) {
@@ -161,22 +213,22 @@ int main() {
   std::string value_str;
   Shop shop(1000000);
   std::vector<Denomination> denominations{
-//      {5, 1},
-//      {10, 7},
-//      {20, 7},
-//      {50, 4},
-      {100, 2},
-      {200, 1},
+      {k5, 0},
+      {k10, 0},
+      {k20, 0},
+      {k50, 0},
+      {k100, 2},
+      {k200, 1},
 
-/*//      {5, 1},
-//      {10, 7},
-      {20, 6},
-      {50, 7},
-      {100, 8},
-//      {200, 1},*/
+      /*{k5, 0},
+      {k10, 0},
+      {k20, 6},
+      {k50, 7},
+      {k100, 8},
+      {k200, 0},*/
   };
 
-  /*auto &num_k5 = denominations[k5].count;
+  auto &num_k5 = denominations[k5].count;
   auto &num_k10 = denominations[k10].count;
   auto &num_k20 = denominations[k20].count;
   auto &num_k50 = denominations[k50].count;
@@ -189,15 +241,13 @@ int main() {
     value_str.erase(value_str.begin() + value_str.find('.'));
     value = std::stoi(value_str);
 
-    auto max_value = static_cast<std::size_t>(5 * num_k5 + 10 * num_k10 + 20 * num_k20 + 50 * num_k50 + 100 * num_k100
-        + 200 * num_k200);
-    Customer customer(1000000, denominations);
+    Customer customer(denominations);
     std::cout.width(3);
     std::cout << std::right << shop.Buy(customer, value) << std::endl;
-  }*/
+  }
 
-  Customer customer(1000000, denominations);
+  /*Customer customer(denominations);
   std::cout.width(3);
-  std::cout << std::right << shop.Buy(customer, value) << std::endl;
+  std::cout << std::right << shop.Buy(customer, value) << std::endl;*/
   return 0;
 }

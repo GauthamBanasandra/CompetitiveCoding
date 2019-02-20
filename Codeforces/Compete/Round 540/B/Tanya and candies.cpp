fcdb46488882ff26e3eb62111fabb56a5676835b
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-02-20 21:59:03 +0530

    [Codeforces] Add "Tanya and candies"

--- Codeforces/Compete/Round 540/B/Tanya and candies.cpp
+++ Codeforces/Compete/Round 540/B/Tanya and candies.cpp
@@ -1,50 +1,111 @@
 #include <vector>
 #include <iostream>
+#include <ios>
 
-int CountGoodCandies(const std::vector<int> &candies)
+namespace round540
 {
-	auto num_good_candies = 0;
-	long long odd_weight = 0, even_weight = 0;
-
-	const auto len = candies.size();
-	for (std::size_t i = 0; i < len; ++i)
+	class Counter
 	{
-		if (i & 1)
+	public:
+		explicit Counter(const std::vector<int> &candies);
+
+		int GoodCandies() const;
+
+	private:
+		long RangeSum(long i_start, long i_end) const;
+
+		long i_last_even_;
+		long i_last_odd_;
+		std::vector<long> cumulative_sum_;
+	};
+
+	Counter::Counter(const std::vector<int>& candies)
+	{
+		const auto len = candies.size();
+		cumulative_sum_.resize(len);
+
+		for (std::size_t i = 0; i < len; ++i)
 		{
-			odd_weight += candies[i];
+			if (i == 0 || i == 1)
+			{
+				cumulative_sum_[i] = candies[i];
+				continue;
+			}
+
+			cumulative_sum_[i] = candies[i] + cumulative_sum_[i - 2];
 		}
-		else
+
+		if (len <= 1)
 		{
-			even_weight += candies[i];
+			return;
 		}
+
+		i_last_even_ = len - (len & 1 ? 1 : 2);
+		i_last_odd_ = len - (len & 1 ? 2 : 1);
 	}
 
-	for (std::size_t i = 0; i < len; ++i)
+	int Counter::GoodCandies() const
 	{
-		auto odd = odd_weight;
-		auto even = even_weight;
-		if (i & 1)
+		auto num_good_candies = 0;
+		const auto len = static_cast<long>(cumulative_sum_.size());
+		if (len == 1)
 		{
-			odd -= candies[i];
-		}
-		else
-		{
-			even -= candies[i];
+			return 1;
 		}
 
-		if (odd == even)
+		for (long i = 0; i < len; ++i)
 		{
-			++num_good_candies;
+			long odd = 0, even = 0;
+			if (i & 1)
+			{
+				odd = RangeSum(1, i - 2);
+				even = RangeSum(0, i - 1);
+				odd += RangeSum(i + 1, i_last_even_);
+				even += RangeSum(i + 2, i_last_odd_);
+			}
+			else
+			{
+				odd = RangeSum(1, i - 1);
+				even = RangeSum(0, i - 2);
+				odd += RangeSum(i + 2, i_last_even_);
+				even += RangeSum(i + 1, i_last_odd_);
+			}
+
+			num_good_candies = odd == even ? num_good_candies + 1 : num_good_candies;
 		}
+		return num_good_candies;
 	}
 
-	return num_good_candies;
+	long Counter::RangeSum(const long i_start, const long i_end) const
+	{
+		const auto len = static_cast<long>(cumulative_sum_.size());
+		if (i_start < 0 || i_end < 0 ||
+			i_start >= len || i_end >= len ||
+			i_end < i_start)
+		{
+			return 0;
+		}
+
+		const auto subtrahend = i_start - 2 < 0 ? 0l : cumulative_sum_[i_start - 2];
+		return cumulative_sum_[i_end] - subtrahend;
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	//std::vector<int> candies{ 4, 8, 8, 7, 8, 4, 4, 5 };
-	std::vector<int> candies{ 2, 3, 4, 2, 2, 3, 2, 2, 4 };
-	std::cout << CountGoodCandies(candies) << std::endl;
+	std::ios::sync_with_stdio(false);
+
+	std::size_t n;
+	std::vector<int> candies;
+
+	std::cin >> n;
+	candies.resize(n);
+	for (std::size_t i = 0; i < n; ++i)
+	{
+		std::cin >> candies[i];
+	}
+
+	round540::Counter counter(candies);
+	std::cout << counter.GoodCandies() << std::endl;
 	return 0;
 }
---
+++ Codeforces/Compete/Round 540/B/run.cmd
@@ -0,0 +1,3 @@
+.\build\Debug\Tanya_and_candies.exe < "Compete\Round 540\B\Tanya and candies1.txt"
+REM .\build\Debug\Tanya_and_candies.exe < "Compete\Round 540\B\Tanya and candies2.txt"
+REM .\build\Debug\Tanya_and_candies.exe < "Compete\Round 540\B\Tanya and candies3.txt"

a18106b2e9cc0db7f65e2bf63bb41d28f9cfde03
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-02-24 21:03:25 +0530

    [UVa] Add "Simple minded hashing" - TLE

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/10912/Simple minded hashing.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/10912/Simple minded hashing.cpp
@@ -8,18 +8,28 @@ class CollisionCounter
 public:
 	CollisionCounter(std::size_t word_len, int rem);
 
-	int Count() { return Count(0, 0, rem_); }
+	int Count();
 private:
 	int Count(std::size_t i_word, std::size_t start_c, int rem);
 
 	const std::size_t word_len_;
 	const int rem_;
-	std::vector<std::vector<int>> memo_;
+	std::vector<std::vector<std::vector<int>>> memo_;
 };
 
 CollisionCounter::CollisionCounter(const std::size_t word_len, const int rem) :word_len_(word_len), rem_(rem)
 {
-	memo_.resize(word_len_, std::vector<int>(27, -1));
+	memo_.resize(word_len_, std::vector<std::vector<int>>(27, std::vector<int>(rem_ + 1, -1)));
+}
+
+int CollisionCounter::Count()
+{
+	auto count = 0;
+	for (std::size_t i = 1; i <= 26; ++i)
+	{
+		count += Count(0, i, rem_ - i);
+	}
+	return count;
 }
 
 int CollisionCounter::Count(const std::size_t i_word, const std::size_t start_c, const int rem)
@@ -34,22 +44,26 @@ int CollisionCounter::Count(const std::size_t i_word, const std::size_t start_c,
 		return 1;
 	}
 
-	auto &memo = memo_[i_word][start_c];
+	auto &memo = memo_[i_word][start_c][rem];
 	if (memo != -1)
 	{
 		return memo;
 	}
 
 	auto count = 0;
-	for (auto c = start_c; c <= 26; ++c)
+	for (auto c = start_c + 1; c <= 26; ++c)
 	{
-		count += Count(i_word + 1, c + 1, rem - (c + 1));
+		count += Count(i_word + 1, c, rem - c);
 	}
 	return memo = count;
 }
 
 int main(int argc, char* argv[])
 {
+	/*CollisionCounter counter(7, 123);
+	std::cout << counter.Count() << std::endl;*/
+
+
 	std::size_t t = 0, l;
 	int s;
 

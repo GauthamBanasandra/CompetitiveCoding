6a652a27086b556100a6c52d478f052e05d6890a
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-02-25 20:46:17 +0530

    [UVa] Add namespace to "How do you add"

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/10943/How do you add.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/10943/How do you add.cpp
@@ -1,62 +1,65 @@
 #include <vector>
 #include <iostream>
 
-class Counter
+namespace uva10943
 {
-public:
-	Counter(std::size_t n, std::size_t k);
-
-	long Count();
-
-private:
-	long Count(std::size_t i_num, int rem);
-
-	const std::size_t n_;
-	const std::size_t k_;
-	std::vector<std::vector<long>> memo_;
-};
-
-Counter::Counter(const std::size_t n, const std::size_t k) : n_(n), k_(k)
-{
-	memo_.resize(k_, std::vector<long>(n_ + 1, -1));
-}
-
-long Counter::Count()
-{
-	auto count = 0l;
-	for (std::size_t i = 0; i <= n_; ++i)
+	class Counter
 	{
-		count += Count(0, n_ - i);
-		count = count % 1000000;
-	}
-	return count;
-}
+	public:
+		Counter(std::size_t n, std::size_t k);
 
-long Counter::Count(const std::size_t i_num, const int rem)
-{
-	if (i_num >= k_ || rem < 0)
+		long Count();
+
+	private:
+		long Count(std::size_t i_num, int rem);
+
+		const std::size_t n_;
+		const std::size_t k_;
+		std::vector<std::vector<long>> memo_;
+	};
+
+	Counter::Counter(const std::size_t n, const std::size_t k) : n_(n), k_(k)
 	{
-		return 0;
+		memo_.resize(k_, std::vector<long>(n_ + 1, -1));
 	}
 
-	if (i_num == k_ - 1 && rem == 0)
+	long Counter::Count()
 	{
-		return 1;
+		auto count = 0l;
+		for (std::size_t i = 0; i <= n_; ++i)
+		{
+			count += Count(0, n_ - i);
+			count = count % 1000000;
+		}
+		return count;
 	}
 
-	auto &memo = memo_[i_num][rem];
-	if (memo != -1)
+	long Counter::Count(const std::size_t i_num, const int rem)
 	{
-		return memo;
-	}
+		if (i_num >= k_ || rem < 0)
+		{
+			return 0;
+		}
 
-	auto count = 0l;
-	for (std::size_t i = 0; i <= n_; ++i)
-	{
-		count += Count(i_num + 1, rem - i);
-		count = count % 1000000;
+		if (i_num == k_ - 1 && rem == 0)
+		{
+			return 1;
+		}
+
+		auto &memo = memo_[i_num][rem];
+		if (memo != -1)
+		{
+			return memo;
+		}
+
+		auto count = 0l;
+		for (std::size_t i = 0; i <= n_; ++i)
+		{
+			count += Count(i_num + 1, rem - i);
+			count = count % 1000000;
+		}
+		return memo = count;
 	}
-	return memo = count;
 }
 
 int main(int argc, char* argv[])
@@ -65,7 +68,7 @@ int main(int argc, char* argv[])
 
 	while (std::cin >> n >> k, n || k)
 	{
-		Counter counter(n, k);
+		uva10943::Counter counter(n, k);
 		std::cout << counter.Count() << std::endl;
 	}
 	return 0;

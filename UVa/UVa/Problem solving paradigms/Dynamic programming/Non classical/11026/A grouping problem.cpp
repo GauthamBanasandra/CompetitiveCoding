61cb07f615de6c3432eb9478c0acdaf625ea9eac
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-03-02 20:00:36 +0530

    [UVa] Debug "A grouping problem"
    * The program will now generate the
       combination alongside the score.

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/11026/A grouping problem.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/11026/A grouping problem.cpp
@@ -15,17 +15,20 @@ public:
 	Group(const std::vector<int> &numbers, std::size_t k);
 
 	void Next();
+	void GenerateCombinations();
 
 private:
+	void SelectNext(std::size_t i_memo, std::size_t i_start_num);
 	ll Next(std::size_t i_memo, std::size_t i_start_num);
 
 	std::vector<std::vector<ll>> memo_;
-	std::vector<ll> memo_selection_;
+	std::vector<int> memo_selection_;
 	const std::vector<int> &numbers_;
 };
 
 Group::Group(const std::vector<int>& numbers, const std::size_t k) :numbers_(numbers)
-{	
+{
+	memo_selection_.resize(k);
 	memo_.resize(k, std::vector<ll>(numbers_.size(), -1));
 }
 
@@ -34,6 +37,34 @@ void Group::Next()
 	for (std::size_t i = 0, len = numbers_.size(); i < len; ++i)
 	{
 		std::cout << i << '\t' << Next(0, i) << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void Group::GenerateCombinations()
+{
+	for (std::size_t i = 0, len = numbers_.size(); i < len; ++i)
+	{
+		SelectNext(0, i);
+	}
+}
+
+void Group::SelectNext(const std::size_t i_memo, const std::size_t i_start_num)
+{
+	if (i_memo >= memo_selection_.size() || i_start_num >= numbers_.size())
+	{
+		return;
+	}
+
+	memo_selection_[i_memo] = numbers_[i_start_num];
+	if (i_memo == memo_selection_.size() - 1)
+	{
+		Print(memo_selection_);
+	}
+
+	for (auto i_num = i_start_num + 1, len = numbers_.size(); i_num < len; ++i_num)
+	{
+		SelectNext(i_memo + 1, i_num);
 	}
 }
 
@@ -56,7 +87,6 @@ ll Group::Next(const std::size_t i_memo, const std::size_t i_start_num)
 		const auto score = Next(i_memo + 1, i_num);
 		max_score = std::max(max_score, score);
 	}
-
 	return memo = max_score != -1 ? max_score * numbers_[i_start_num] : numbers_[i_start_num];
 }
 
@@ -66,5 +96,6 @@ int main(int argc, char* argv[])
 
 	Group group(numbers, 2);
 	group.Next();
+	group.GenerateCombinations();
 	return 0;
 }

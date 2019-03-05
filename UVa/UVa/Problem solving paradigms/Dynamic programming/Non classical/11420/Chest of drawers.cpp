09f5ffb2bb6528241a0a17e530f8ddc0444580f3
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-03-05 20:56:32 +0530

    [UVa] Add Java build "Chest of drawers"

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/11420/Chest of drawers.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/11420/Chest of drawers.cpp
@@ -41,6 +41,35 @@ Counter::Counter(const std::size_t n, const int s) :n_(n), s_(s)
  | L L U |	and will return immediately without exploring the subsequent positions
  | L L L |
   -------
+
+ * The code looks like this for "rem" logic
+Counter::Counter(const std::size_t n, const std::size_t s) :n_(n), s_(s)
+{
+	memo_.resize(n_, std::vector<std::vector<long>>(2, std::vector<long>(s_ + 1, -1)));
+}
+
+long Counter::Count(const std::size_t i, const bool is_prev_lock, const int rem)
+{
+	if (rem == 0)
+	{
+		return 1;
+	}
+
+	if (i >= n_ || rem < 0)
+	{
+		return 0;
+	}
+
+	auto& memo = memo_[i][is_prev_lock ? 1 : 0][rem];
+	if (memo != -1)
+	{
+		return memo;
+	}
+
+	const auto unlocked = Count(i + 1, false, rem);
+	const auto locked = Count(i + 1, true, rem - (is_prev_lock ? 1 : 0));
+	return memo = unlocked + locked;
+}
  */
 ll Counter::Count(const std::size_t i, const bool is_prev_lock, const int s)
 {

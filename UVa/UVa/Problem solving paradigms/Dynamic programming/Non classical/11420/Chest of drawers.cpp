dd83f79ae165eca055c6e34eba1e3429cf923ab5
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-03-09 16:47:01 +0530

    [UVa] Add namespace to "Chest of drawers"

--- UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/11420/Chest of drawers.cpp
+++ UVa/UVa/Problem solving paradigms/Dynamic programming/Non classical/11420/Chest of drawers.cpp
@@ -1,28 +1,30 @@
 #include <vector>
 #include <iostream>
 
-using ll = long long;
-
-class Counter
+namespace uva11420
 {
-public:
-	Counter(std::size_t n, int s);
-	ll Count() { return Count(0, true, 0); }
+	using ll = long long;
 
-private:
-	ll Count(std::size_t i, bool is_prev_lock, int s);
+	class Counter
+	{
+	public:
+		Counter(std::size_t n, int s);
+		ll Count() { return Count(0, true, 0); }
 
-	const std::size_t n_;
-	const int s_;
-	std::vector<std::vector<std::vector<ll>>> memo_;
-};
+	private:
+		ll Count(std::size_t i, bool is_prev_lock, int s);
 
-Counter::Counter(const std::size_t n, const int s) :n_(n), s_(s)
-{
-	memo_.resize(n_, std::vector<std::vector<ll>>(2, std::vector<ll>(n_ + 1, -1)));
-}
+		const std::size_t n_;
+		const int s_;
+		std::vector<std::vector<std::vector<ll>>> memo_;
+	};
 
-/*
+	Counter::Counter(const std::size_t n, const int s) :n_(n), s_(s)
+	{
+		memo_.resize(n_, std::vector<std::vector<ll>>(2, std::vector<ll>(n_ + 1, -1)));
+	}
+
+	/*
  * Using "rem" all the time isn't going to work.
  * By "rem", I mean passing the remaining and subtracting it till "rem" reaches 0.
  * 
@@ -71,36 +73,37 @@ long Counter::Count(const std::size_t i, const bool is_prev_lock, const int rem)
 	return memo = unlocked + locked;
 }
  */
-ll Counter::Count(const std::size_t i, const bool is_prev_lock, const int s)
-{
-	if (i >= n_)
+	ll Counter::Count(const std::size_t i, const bool is_prev_lock, const int s)
 	{
-		if (s == s_)
+		if (i >= n_)
 		{
-			return 1;
+			if (s == s_)
+			{
+				return 1;
+			}
+			return 0;
 		}
-		return 0;
-	}
 
-	auto& memo = memo_[i][is_prev_lock ? 1 : 0][s];
-	if (memo != -1)
-	{
-		return memo;
-	}
+		auto& memo = memo_[i][is_prev_lock ? 1 : 0][s];
+		if (memo != -1)
+		{
+			return memo;
+		}
 
-	const auto unlocked = Count(i + 1, false, s);
-	const auto locked = Count(i + 1, true, s + (is_prev_lock ? 1 : 0));
-	return memo = unlocked + locked;
+		const auto unlocked = Count(i + 1, false, s);
+		const auto locked = Count(i + 1, true, s + (is_prev_lock ? 1 : 0));
+		return memo = unlocked + locked;
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	ll n;
+	uva11420::ll n;
 	int s;
 
 	while (std::cin >> n >> s, n != -1 && s != -1)
 	{
-		Counter counter(n, s);
+		uva11420::Counter counter(n, s);
 		std::cout << counter.Count() << std::endl;
 	}
 	return 0;

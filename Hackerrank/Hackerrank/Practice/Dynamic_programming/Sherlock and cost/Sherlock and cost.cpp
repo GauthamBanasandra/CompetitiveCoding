d5b429387f84b8545104428cb383233dddf06c6a
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-03-13 21:52:46 +0530

    [Hackerrank] Add "Sherlock and cost" - TLE

--- Hackerrank/Hackerrank/Practice/Dynamic_programming/Sherlock and cost/Sherlock and cost.cpp
+++ Hackerrank/Hackerrank/Practice/Dynamic_programming/Sherlock and cost/Sherlock and cost.cpp
@@ -1,62 +1,63 @@
+// TLE
+
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <valarray>
 
-class CostEstimator
+class Maximizer
 {
 public:
-	explicit CostEstimator(const std::vector<int> &numbers);
+	Maximizer(const std::vector<int> &numbers);
 
-	int Estimate();
+	long Maximize() { return Maximize(0, 0); }
 
 private:
-	std::vector<std::vector<int>> memo_;
-	std::vector<std::vector<int>> possibilities_;
+	long Maximize(std::size_t i_num, int prev_num);
+
 	const std::vector<int> &numbers_;
+	std::vector<std::vector<long>> memo_;
 };
 
-CostEstimator::CostEstimator(const std::vector<int>& numbers) :numbers_(numbers)
+Maximizer::Maximizer(const std::vector<int>& numbers) : numbers_(numbers)
 {
-	memo_.resize(numbers_.size());
-	possibilities_.resize(numbers_.size());
-	for (std::size_t i = 0, len = memo_.size(); i < len; ++i)
-	{
-		memo_[i].resize(static_cast<std::size_t>(numbers_[i]));
-		possibilities_[i].resize(static_cast<std::size_t>(numbers_[i]));
-		for (auto j = 0; j < numbers_[i]; ++j)
-		{
-			possibilities_[i][j] = j + 1;
-		}
-	}
+	memo_.resize(numbers_.size(),
+		std::vector<long>(*std::max_element(numbers_.begin(), numbers_.end()) + 1, -1));
 }
 
-int CostEstimator::Estimate()
+long Maximizer::Maximize(const std::size_t i_num, const int prev_num)
 {
-	for (std::size_t i = 1, len = memo_.size(); i < len; ++i)
+	if (i_num >= numbers_.size())
 	{
-		for (std::size_t i_curr = 0, curr_len = possibilities_[i].size(); i_curr < curr_len; ++i_curr)
-		{
-			for (std::size_t i_prev = 0, prev_len = possibilities_[i - 1].size(); i_prev < prev_len; ++i_prev)
-			{
-				auto cost = std::abs(possibilities_[i][i_curr] - possibilities_[i - 1][i_prev]);
-				cost += memo_[i - 1][i_prev];
-				memo_[i][i_curr] = std::max(cost, memo_[i][i_curr]);
-			}
-		}
+		return 0;
 	}
 
-	auto cost = 0;
-	for (const auto value : memo_.back())
+	auto &memo = memo_[i_num][prev_num];
+	if (memo != -1)
 	{
-		cost = std::max(cost, value);
+		return memo;
 	}
-	return cost;
+
+	long max_score = 0;
+	for (auto num = 1, max_num = numbers_[i_num]; num <= max_num; ++num)
+	{
+		auto score = Maximize(i_num + 1, num);
+		if (i_num != 0)
+		{
+			score += std::abs(prev_num - num);
+		}
+		max_score = std::max(max_score, score);
+	}
+	return memo = max_score;
 }
 
 int main(int argc, char* argv[])
 {
 	std::size_t t, num_len;
-	std::vector<int> numbers{ 10, 1, 10, 1, 10 };
+	std::vector<int> numbers{ 1,2,3 };
+
+	/*Maximizer maximizer(numbers);
+	std::cout << maximizer.Maximize() << std::endl;*/
 
 	std::cin >> t;
 	while (t--)
@@ -68,8 +69,8 @@ int main(int argc, char* argv[])
 			std::cin >> numbers[i];
 		}
 
-		CostEstimator estimator(numbers);
-		std::cout << estimator.Estimate() << std::endl;
+		Maximizer maximizer(numbers);
+		std::cout << maximizer.Maximize() << std::endl;
 	}
 	return 0;
 }

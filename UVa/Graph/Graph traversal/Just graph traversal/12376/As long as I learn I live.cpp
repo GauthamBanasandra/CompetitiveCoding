74f92457e567db93f21e67253ee1a6d97f010029
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-03-30 21:05:00 +0530

    [UVa] Add "As long as I learn I live"

--- UVa/Graph/Graph traversal/Just graph traversal/12376/As long as I learn I live.cpp
+++ UVa/Graph/Graph traversal/Just graph traversal/12376/As long as I learn I live.cpp
@@ -1,105 +1,104 @@
-// WIP
-
 #include <vector>
 #include <queue>
 #include <algorithm>
 #include <iostream>
+#include <ios>
 
-struct Node
+namespace uva12376
 {
-	Node(const int id, const int value) :id(id), value(value) {}
-
-	int id{ 0 };
-	int value{ 0 };
-};
-
-class Traversal
-{
-public:
-	explicit Traversal(int num_nodes,
-		const std::vector<std::pair<int, int>> &edge_list, const std::vector<int> &value);
-
-	std::pair<int, int> Traverse();
-
-private:
-	void DFS(int node, const int
-		value_sum, int& max_value,
-		int& last_node);
-
-	std::vector<std::vector<Node>> adj_list_;
-	std::vector<short> visited_;
-};
-
-Traversal::Traversal(const int num_nodes,
-	const std::vector<std::pair<int, int>>& edge_list, const std::vector<int> &value)
-{
-	adj_list_.resize(num_nodes);
-	visited_.resize(num_nodes);
-	for (const auto& edge : edge_list)
+	struct Node
 	{
-		const auto&[u, v] = edge;
-		adj_list_[u].emplace_back(v, value[v]);
-	}
+		Node(const int id, const int value) :id(id), value(value) {}
 
-	for (auto &nodes : adj_list_)
+		int id{ 0 };
+		int value{ 0 };
+	};
+
+	class Traversal
 	{
-		std::sort(nodes.begin(), nodes.end(),
-			[](const Node &a, const Node&b)->bool
+	public:
+		explicit Traversal(int num_nodes,
+			const std::vector<std::pair<int, int>> &edge_list, const std::vector<int> &value);
+
+		std::pair<int, int> Traverse();
+
+	private:
+		void DFS(int node, const int
+			value_sum, int& max_value,
+			int& last_node, bool& is_done);
+
+		std::vector<std::vector<Node>> adj_list_;
+		std::vector<short> visited_;
+	};
+
+	Traversal::Traversal(const int num_nodes,
+		const std::vector<std::pair<int, int>>& edge_list, const std::vector<int> &value)
+	{
+		adj_list_.resize(num_nodes);
+		visited_.resize(num_nodes);
+		for (const auto& edge : edge_list)
 		{
-			return a.value > b.value;
-		});
-	}
-}
-
-std::pair<int, int> Traversal::Traverse()
-{
-	auto max_value = 0;
-	auto last_node = 0;
-	DFS(0, 0, max_value, last_node);
-	return std::make_pair(max_value, last_node);
-}
-
-void Traversal::DFS(const int node, const int value_sum,
-	int& max_value, int& last_node)
-{
-	visited_[node] = 1;
-	auto is_leaf = true;
-	for (const auto& adj_node : adj_list_[node])
-	{
-		is_leaf = false;
-		if (visited_[adj_node.id] != 0)
-		{
-			continue;
+			const auto&[u, v] = edge;
+			adj_list_[u].emplace_back(v, value[v]);
 		}
 
-		DFS(adj_node.id, value_sum + adj_node.value, max_value, last_node);
+		for (auto &nodes : adj_list_)
+		{
+			std::sort(nodes.begin(), nodes.end(),
+				[](const Node &a, const Node&b)->bool
+			{
+				return a.value > b.value;
+			});
+		}
 	}
 
-	if (is_leaf && value_sum > max_value)
+	std::pair<int, int> Traversal::Traverse()
 	{
-		max_value = value_sum;
-		last_node = node;
+		auto max_value = 0;
+		auto last_node = 0;
+		bool is_done = false;
+		DFS(0, 0, max_value, last_node, is_done);
+		return std::make_pair(max_value, last_node);
+	}
+
+	void Traversal::DFS(const int node, const int value_sum,
+		int& max_value, int& last_node, bool& is_done)
+	{
+		visited_[node] = 1;
+		auto is_leaf = true;
+		for (const auto& adj_node : adj_list_[node])
+		{
+			is_leaf = false;
+			if (visited_[adj_node.id] != 0)
+			{
+				continue;
+			}
+
+			DFS(adj_node.id, value_sum + adj_node.value, max_value, last_node, is_done);
+			if (is_done)
+			{
+				return;
+			}
+		}
+
+		if (is_leaf && value_sum > max_value)
+		{
+			max_value = value_sum;
+			last_node = node;
+			is_done = true;
+		}
 	}
 }
 
 int main(int argc, char* argv[])
 {
+	std::ios::sync_with_stdio(false);
+
 	int t;
 	int n;
 	int m;
-	std::vector<std::pair<int, int>> edge_list{
-{5, 4},
-{5, 3},
-{1, 5},
-{0, 1},
-{0, 2},
-{2, 1},
-	};
-
-	std::vector<int> values{ 0,8,9,2,7,5 };
-	/*Traversal traversal(6, edge_list, values);
-	auto[max_value, last_node] = traversal.Traverse();
-	std::cout << max_value << ' ' << last_node << std::endl;*/
+	std::vector<int> values;
+	std::vector<std::pair<int, int>> edge_list;
 
 	std::cin >> t;
 	for (auto i = 1; i <= t; ++i)
@@ -117,7 +116,7 @@ int main(int argc, char* argv[])
 			std::cin >> edge_list[j].first >> edge_list[j].second;
 		}
 
-		Traversal traversal(n, edge_list, values);
+		uva12376::Traversal traversal(n, edge_list, values);
 		auto[max_value, last_node] = traversal.Traverse();
 		std::cout << "Case " << i << ": " << max_value << ' ' << last_node << std::endl;
 	}
--- UVa/Graph/Graph traversal/Just graph traversal/12376/run.cmd
+++ UVa/Graph/Graph traversal/Just graph traversal/12376/run.cmd
@@ -1,2 +1,4 @@
 REM .\build\Debug\As_long_as_I_learn_I_live.exe < "Graph\Graph traversal\Just graph traversal\12376\As long as I learn I live.txt"
 .\build\Debug\As_long_as_I_learn_I_live.exe < "Graph\Graph traversal\Just graph traversal\12376\As long as I learn I live1.txt" | clip
+REM .\build\Debug\As_long_as_I_learn_I_live.exe < "Graph\Graph traversal\Just graph traversal\12376\As long as I learn I live2.txt" | clip
+REM .\build\Debug\As_long_as_I_learn_I_live.exe < "Graph\Graph traversal\Just graph traversal\12376\As long as I learn I live3.txt" | clip

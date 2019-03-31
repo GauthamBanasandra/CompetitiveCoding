01dd7aec62753f0a90ee87300c0ab8756d835dad
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-03-31 20:31:30 +0530

    [Codeforces] Add "Parity alternated deletions"

---
+++ Codeforces/Compete/Round 550/B/Parity alternated deletions.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using ll = long long;
+
+ll MinimumSum(const std::vector<long> &numbers)
+{
+	std::vector<long> even, odd;
+	for (const auto number : numbers)
+	{
+		(number & 1 ? odd : even).emplace_back(number);
+	}
+
+	std::sort(odd.begin(), odd.end());
+	std::sort(even.begin(), even.end());
+
+	if (odd.size() == even.size() ||
+		abs(static_cast<long>(odd.size()) - static_cast<long>(even.size())) == 1)
+	{
+		return 0;
+	}
+
+	if (odd.size() < even.size())
+	{
+		ll sum = 0;
+		for (std::size_t i = 0, len = even.size() - odd.size(); i < len - 1; ++i)
+		{
+			sum += even[i];
+		}
+		return sum;
+	}
+
+	ll sum = 0;
+	for (std::size_t i = 0, len = odd.size() - even.size(); i < len - 1; ++i)
+	{
+		sum += odd[i];
+	}
+	return sum;
+}
+
+int main(int argc, char* argv[])
+{
+	std::size_t n;
+	std::cin >> n;
+
+	std::vector<long> numbers(n);
+	for (std::size_t i = 0; i < n; ++i)
+	{
+		std::cin >> numbers[i];
+	}
+
+	std::cout << MinimumSum(numbers) << std::endl;
+	return 0;
+}

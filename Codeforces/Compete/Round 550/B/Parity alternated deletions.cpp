60caa729200141975465f1670c22d8ac479cbc24
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-03-31 20:33:13 +0530

    [Codeforces] Add namespace "Parity alternated deletions"

--- Codeforces/Compete/Round 550/B/Parity alternated deletions.cpp
+++ Codeforces/Compete/Round 550/B/Parity alternated deletions.cpp
@@ -2,41 +2,44 @@
 #include <algorithm>
 #include <iostream>
 
-using ll = long long;
-
-ll MinimumSum(const std::vector<long> &numbers)
+namespace round550
 {
-	std::vector<long> even, odd;
-	for (const auto number : numbers)
-	{
-		(number & 1 ? odd : even).emplace_back(number);
-	}
+	using ll = long long;
 
-	std::sort(odd.begin(), odd.end());
-	std::sort(even.begin(), even.end());
-
-	if (odd.size() == even.size() ||
-		abs(static_cast<long>(odd.size()) - static_cast<long>(even.size())) == 1)
+	ll MinimumSum(const std::vector<long> &numbers)
 	{
-		return 0;
-	}
-
-	if (odd.size() < even.size())
-	{
-		ll sum = 0;
-		for (std::size_t i = 0, len = even.size() - odd.size(); i < len - 1; ++i)
+		std::vector<long> even, odd;
+		for (const auto number : numbers)
 		{
-			sum += even[i];
+			(number & 1 ? odd : even).emplace_back(number);
+		}
+
+		std::sort(odd.begin(), odd.end());
+		std::sort(even.begin(), even.end());
+
+		if (odd.size() == even.size() ||
+			abs(static_cast<long>(odd.size()) - static_cast<long>(even.size())) == 1)
+		{
+			return 0;
+		}
+
+		if (odd.size() < even.size())
+		{
+			ll sum = 0;
+			for (std::size_t i = 0, len = even.size() - odd.size(); i < len - 1; ++i)
+			{
+				sum += even[i];
+			}
+			return sum;
+		}
+
+		ll sum = 0;
+		for (std::size_t i = 0, len = odd.size() - even.size(); i < len - 1; ++i)
+		{
+			sum += odd[i];
 		}
 		return sum;
 	}
-
-	ll sum = 0;
-	for (std::size_t i = 0, len = odd.size() - even.size(); i < len - 1; ++i)
-	{
-		sum += odd[i];
-	}
-	return sum;
 }
 
 int main(int argc, char* argv[])
@@ -50,6 +53,6 @@ int main(int argc, char* argv[])
 		std::cin >> numbers[i];
 	}
 
-	std::cout << MinimumSum(numbers) << std::endl;
+	std::cout << round550::MinimumSum(numbers) << std::endl;
 	return 0;
 }

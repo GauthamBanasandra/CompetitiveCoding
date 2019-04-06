6f80883484e01fe8786d183ab032b8e2d847ef54
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-04-06 22:26:29 +0530

    [Codechef] Add "Fencing"

--- Codechef/Compete/2019/April challenge/FENCE/Fencing.cpp
+++ Codechef/Compete/2019/April challenge/FENCE/Fencing.cpp
@@ -1,7 +1,6 @@
-// TLE
-
 #include <unordered_set>
 #include <vector>
+#include <string>
 #include <iostream>
 
 namespace april_challenge_2019
@@ -13,8 +12,7 @@ namespace april_challenge_2019
 		Pos() = default;
 		Pos(const ll x, const ll y) :x(x), y(y) {}
 
-		bool operator==(const Pos& other) const { return x == other.x && y == other.y; }
-		std::size_t operator()(const Pos& pos) const { return x * 1'000'000'000 + y; }
+		std::string GetHash() const { return std::to_string(x) + " " + std::to_string(y); }
 
 		ll x{ 0 };
 		ll y{ 0 };
@@ -23,7 +21,7 @@ namespace april_challenge_2019
 	class Traversal
 	{
 	public:
-		Traversal(ll n, ll m, const std::unordered_set<Pos, Pos>& plants_pos);
+		Traversal(ll n, ll m, const std::vector<Pos>& plants_pos);
 
 		ll GetFences();
 
@@ -36,17 +34,21 @@ namespace april_challenge_2019
 
 		const ll num_rows_;
 		const ll num_columns_;
-		const std::unordered_set<Pos, Pos>& plants_pos_;
-		std::unordered_set<Pos, Pos> visited_;
-
+		const std::vector<Pos>& plants_pos_;
+		std::unordered_set<std::string> visited_;
+		std::unordered_set<std::string> plants_pos_set_;
 		static std::vector<std::pair<int, int>> directions_;
 	};
 
 	std::vector<std::pair<int, int>> Traversal::directions_;
 
-	Traversal::Traversal(const ll n, const ll m, const std::unordered_set<Pos, Pos>& plants_pos) :
+	Traversal::Traversal(const ll n, const ll m, const std::vector<Pos>& plants_pos) :
 		num_rows_(n), num_columns_(m), plants_pos_(plants_pos)
 	{
+		for (const auto& pos : plants_pos_)
+		{
+			plants_pos_set_.insert(pos.GetHash());
+		}
 		if (directions_.empty())
 		{
 			directions_ = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
@@ -58,7 +60,7 @@ namespace april_challenge_2019
 		ll count = 0;
 		for (const auto& pos : plants_pos_)
 		{
-			if (visited_.find(pos) == visited_.end())
+			if (visited_.find(pos.GetHash()) == visited_.end())
 			{
 				count += Next(pos);
 			}
@@ -68,15 +70,15 @@ namespace april_challenge_2019
 
 	ll Traversal::Next(const Pos pos)
 	{
-		if (IsOutside(pos) || plants_pos_.find(pos) == plants_pos_.end())
+		if (IsOutside(pos) || plants_pos_set_.find(pos.GetHash()) == plants_pos_set_.end())
 		{
 			return 1;
 		}
-		if (visited_.find(pos) != visited_.end())
+		if (visited_.find(pos.GetHash()) != visited_.end())
 		{
 			return 0;
 		}
-		visited_.insert(pos);
+		visited_.insert(pos.GetHash());
 
 		ll count = 0;
 		for (const auto& direction : directions_)
@@ -92,19 +94,17 @@ int main(int argc, char* argv[])
 	int t;
 	april_challenge_2019::ll n;
 	april_challenge_2019::ll m;
-	april_challenge_2019::ll x;
-	april_challenge_2019::ll y;
 	std::size_t k;
+	std::vector<april_challenge_2019::Pos> plants_pos;
 
 	std::cin >> t;
 	while (t--)
 	{
 		std::cin >> n >> m >> k;
-		std::unordered_set<april_challenge_2019::Pos, april_challenge_2019::Pos> plants_pos;
+		plants_pos.resize(k);
 		for (std::size_t i = 0; i < k; ++i)
 		{
-			std::cin >> x >> y;
-			plants_pos.emplace(x, y);
+			std::cin >> plants_pos[i].x >> plants_pos[i].y;
 		}
 		april_challenge_2019::Traversal traversal(n, m, plants_pos);
 		std::cout << traversal.GetFences() << std::endl;

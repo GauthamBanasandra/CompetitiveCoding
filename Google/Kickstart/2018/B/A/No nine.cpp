8020410a6003fb429998430de1f3c25f7496c038
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-04-07 20:18:25 +0530

    [Kickstart] Add documentation
    * Added documentation to "No nine".

--- Google/Kickstart/2018/B/A/No nine.cpp
+++ Google/Kickstart/2018/B/A/No nine.cpp
@@ -1,8 +1,21 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <ios>
 
 using ll = long long;
+
+class Number
+{
+public:
+	Number(const std::string& number) : number_(number) {}
+
+	int operator[](const std::size_t i) const { return number_[i] - '0'; }
+	std::size_t Size() const { return number_.size(); }
+
+private:
+	const std::string& number_;
+};
 
 class Counter
 {
@@ -12,34 +25,39 @@ public:
 	ll CountNo9();
 
 private:
-	ll Count(int i_digit, int digit_sum, bool no_limit);
-	int GetDigit(const std::size_t i) const { return number_[i] - '0'; }
+	ll Count(std::size_t i_digit, int digit_sum, bool no_limit);
 
-	const std::string& number_;
+	const Number number_;
 	std::vector<std::vector<std::vector<ll>>> memo_;
 };
 
 Counter::Counter(const std::string& number) : number_(number)
 {
-	memo_.resize(number_.size(),
-		std::vector<std::vector<ll>>(10,
+	// DP table structure - 
+	// num_digits x max_value_of_a_digit(= 8) x can_current_digit_go_up_to_8
+	memo_.resize(number_.Size(),
+		std::vector<std::vector<ll>>(9,
 			std::vector<ll>(2, -1)));
 }
 
 ll Counter::CountNo9()
 {
-	const auto limit = GetDigit(0);
 	ll count = 0;
-	for (auto i = 0; i <= limit; ++i)
+	for (auto i = 0; i <= number_[0]; ++i)
 	{
-		count += Count(1, i % 9, i < limit);
+		count += Count(1, i % 9, i < number_[0]);
 	}
 	return count;
 }
 
-ll Counter::Count(const int i_digit, const int digit_sum, const bool no_limit)
+// i_digit ->	The current digit
+// digit_sum ->	Sum % 9 of the digits so far. Useful for checking if a number is divisible by 9
+// no_limit ->	If digit i hasn't reached its limit(i.e. its digit value), it implies that all
+//				the subsequent (i+1), (i+2) ... last_digit can loop over [0, 8]. Otherwise, they
+//				can only loop only over [0, whatever_is_the_digit_limit]
+ll Counter::Count(const std::size_t i_digit, const int digit_sum, const bool no_limit)
 {
-	if (i_digit >= static_cast<int>(number_.size()))
+	if (i_digit >= number_.Size())
 	{
 		return digit_sum != 0 ? 1 : 0;
 	}
@@ -50,7 +68,7 @@ ll Counter::Count(const int i_digit, const int digit_sum, const bool no_limit)
 		return memo;
 	}
 
-	const auto limit = GetDigit(i_digit);
+	const auto limit = number_[i_digit];
 	ll count = 0;
 	for (auto i = 0; i <= (no_limit ? 8 : limit); ++i)
 	{
@@ -62,6 +80,8 @@ ll Counter::Count(const int i_digit, const int digit_sum, const bool no_limit)
 
 int main(int argc, char* argv[])
 {
+	std::ios::sync_with_stdio(false);
+
 	int t;
 	std::string f;
 	std::string l;

d72bec6f8c6b39e8a213aed075c36360320ef607
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-04-07 22:04:05 +0530

    [Codechef] Add "Maximum remaining"

--- Codechef/Compete/2019/April challenge/MAXREM/Maximum remaining.cpp
+++ Codechef/Compete/2019/April challenge/MAXREM/Maximum remaining.cpp
@@ -1,30 +1,35 @@
-// WA
-
 #include <queue>
 #include <iostream>
 
-using ll = long long;
-
-ll GetMaximumRemaining(const int n)
+namespace april_challenge_2019
 {
-	ll number;
-	std::priority_queue<ll> numbers;
-	for (auto i = 0; i < n; ++i)
-	{
-		std::cin >> number;
-		numbers.emplace(number);
-	}
+	using ll = long long;
 
-	const auto biggest = numbers.top();
-	numbers.pop();
-	const auto second_biggest = numbers.top();
-	return biggest == second_biggest ? 0 : second_biggest;
+	ll GetMaximumRemaining(const int n)
+	{
+		ll number;
+		std::priority_queue<ll> numbers;
+		for (auto i = 0; i < n; ++i)
+		{
+			std::cin >> number;
+			numbers.emplace(number);
+		}
+
+		const auto biggest = numbers.top();
+		numbers.pop();
+
+		while (!numbers.empty() && numbers.top() == biggest)
+		{
+			numbers.pop();
+		}
+		return numbers.empty() ? 0 : numbers.top();
+	}
 }
 
 int main(int argc, char* argv[])
 {
 	int n;
 	std::cin >> n;
-	std::cout << GetMaximumRemaining(n) << std::endl;
+	std::cout << april_challenge_2019::GetMaximumRemaining(n) << std::endl;
 	return 0;
 }

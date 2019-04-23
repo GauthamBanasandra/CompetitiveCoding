fb4cfbf7b9d991eb070039e5fcbc44823ff5d7eb
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-04-23 06:34:14 +0530

    [UVa] Add namespace
    * Added namespace to "Montesco vs capuleto"

--- UVa/Graph/Graph traversal/Bipartite graph check/10505/Montesco vs capuleto.cpp
+++ UVa/Graph/Graph traversal/Bipartite graph check/10505/Montesco vs capuleto.cpp
@@ -1,75 +1,76 @@
-// WIP
-
 #include <vector>
 #include <algorithm>
 #include <queue>
 #include <iostream>
 
-class BipartiteCheck
+namespace uva10505
 {
-public:
-	BipartiteCheck(const int num_nodes, const std::vector<std::vector<int>>& adj_list)
-		:num_nodes_(num_nodes + 1), adj_list_(adj_list) {}
-
-	int CountFriends() const;
-
-private:
-	int CountFriends(int start_node, std::vector<int>& node_colour) const;
-
-	const int num_nodes_;
-	const std::vector<std::vector<int>>& adj_list_;
-};
-
-int BipartiteCheck::CountFriends() const
-{
-	auto total_count = 0;
-	std::vector<int> node_colour(num_nodes_, -1);
-	for (auto i = 1; i < num_nodes_; ++i)
+	class BipartiteCheck
 	{
-		if (node_colour[i] != -1)
-		{
-			continue;
-		}
+	public:
+		BipartiteCheck(const int num_nodes, const std::vector<std::vector<int>>& adj_list)
+			:num_nodes_(num_nodes + 1), adj_list_(adj_list) {}
 
-		const auto count = CountFriends(i, node_colour);
-		if (count != -1)
-		{
-			total_count += count;
-		}
-	}
-	return total_count;
-}
+		int CountFriends() const;
 
-int BipartiteCheck::CountFriends(const int start_node,
-	std::vector<int>& node_colour) const
-{
-	std::queue<int> order;
-	order.push(start_node);
-	node_colour[start_node] = 0;
-	std::vector<int> num_colour(2);
-	auto is_bipartite = true;
+	private:
+		int CountFriends(int start_node, std::vector<int>& node_colour) const;
 
-	while (!order.empty())
+		const int num_nodes_;
+		const std::vector<std::vector<int>>& adj_list_;
+	};
+
+	int BipartiteCheck::CountFriends() const
 	{
-		const auto node = order.front();
-		order.pop();
-
-		++num_colour[node_colour[node]];
-
-		for (const auto adj_node : adj_list_[node])
+		auto total_count = 0;
+		std::vector<int> node_colour(num_nodes_, -1);
+		for (auto i = 1; i < num_nodes_; ++i)
 		{
-			if (node_colour[adj_node] == -1)
+			if (node_colour[i] != -1)
 			{
-				node_colour[adj_node] = 1 - node_colour[node];
-				order.push(adj_node);
+				continue;
 			}
-			else if (node_colour[node] == node_colour[adj_node])
+
+			const auto count = CountFriends(i, node_colour);
+			if (count != -1)
 			{
-				is_bipartite = false;
+				total_count += count;
 			}
 		}
+		return total_count;
 	}
-	return is_bipartite ? *std::max_element(num_colour.begin(), num_colour.end()) : -1;
+
+	int BipartiteCheck::CountFriends(const int start_node,
+	                                 std::vector<int>& node_colour) const
+	{
+		std::queue<int> order;
+		order.push(start_node);
+		node_colour[start_node] = 0;
+		std::vector<int> num_colour(2);
+		auto is_bipartite = true;
+
+		while (!order.empty())
+		{
+			const auto node = order.front();
+			order.pop();
+
+			++num_colour[node_colour[node]];
+
+			for (const auto adj_node : adj_list_[node])
+			{
+				if (node_colour[adj_node] == -1)
+				{
+					node_colour[adj_node] = 1 - node_colour[node];
+					order.push(adj_node);
+				}
+				else if (node_colour[node] == node_colour[adj_node])
+				{
+					is_bipartite = false;
+				}
+			}
+		}
+		return is_bipartite ? *std::max_element(num_colour.begin(), num_colour.end()) : -1;
+	}
 }
 
 int main(int argc, char* argv[])
@@ -100,7 +101,7 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		BipartiteCheck check(num_nodes, adj_list);
+		uva10505::BipartiteCheck check(num_nodes, adj_list);
 		std::cout << check.CountFriends() << std::endl;
 	}
 	return 0;

58d9ee0bb074803318c94d3524bc8849b6755c9d
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-05-11 17:50:56 +0530

    [Leetcode] Add documentation
    * Documented "Longest palindromic substring"

--- Leetcode/Practice/Medium/5/Longest palindromic substring.cpp
+++ Leetcode/Practice/Medium/5/Longest palindromic substring.cpp
@@ -8,6 +8,7 @@ std::string LongestPalindrome(const std::string& str)
 	{
 		for (auto i = i_start, rev_idx = 1; i < i_end - rev_idx; ++i, ++rev_idx)
 		{
+			// Just check if the mirror element is the same
 			if (str[i] != str[i_end - rev_idx])
 			{
 				return false;
@@ -19,6 +20,8 @@ std::string LongestPalindrome(const std::string& str)
 	const auto str_len = static_cast<int>(str.size());
 	for (auto len = str_len; len > 0; --len)
 	{
+		// Keep checking if a palindrome of length str_len exists
+		// If not, then check if a palindrome of length str_len - 1, str_len - 2 exists and so on
 		for (auto i = 0; i <= str_len - len; ++i)
 		{
 			if (is_palindrome(i, i + len))

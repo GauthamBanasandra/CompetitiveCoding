8b564e7bf3d244bf70020fcaadbd15eef917d006
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-05-11 17:43:02 +0530

    [Leetcode] Add "Longest palindromic substring"

--- Leetcode/Practice/Medium/5/Longest palindromic substring.cpp
+++ Leetcode/Practice/Medium/5/Longest palindromic substring.cpp
@@ -1,110 +1,49 @@
-//
-// Created by Gautham Banasandra on 21/09/18.
-//
-
-// WA
-
 #include <string>
-#include <algorithm>
 #include <iostream>
 #include <cassert>
 
-struct PalindromeInfo {
-  PalindromeInfo() : length(0) {}
-  PalindromeInfo(size_t length) : length(length) {}
-  PalindromeInfo(size_t length,
-                 std::string::iterator begin,
-                 std::string::iterator end)
-      : length(length), begin(begin), end(end) {}
+std::string LongestPalindrome(const std::string& str)
+{
+	const auto is_palindrome = [&str](const int i_start, const int i_end)->bool
+	{
+		for (auto i = i_start, rev_idx = 1; i < i_end - rev_idx; ++i, ++rev_idx)
+		{
+			if (str[i] != str[i_end - rev_idx])
+			{
+				return false;
+			}
+		}
+		return true;
+	};
 
-  std::size_t length;
-  std::string::iterator begin;
-  std::string::iterator end;
-};
+	const auto str_len = static_cast<int>(str.size());
+	for (auto len = str_len; len > 0; --len)
+	{
+		for (auto i = 0; i <= str_len - len; ++i)
+		{
+			if (is_palindrome(i, i + len))
+			{
+				return str.substr(i, len);
+			}
+		}
+	}
+
+	//assert(false && "Smallest palindrome must be of length 1");
+	return "";
+}
 
 class Solution {
- public:
-  std::string longestPalindrome(std::string s) const;
-
- private:
-  std::string LongestPalindrome(std::string &s) const;
-  PalindromeInfo TryEven(std::string &s, std::string::iterator &it) const;
-  PalindromeInfo TryOdd(std::string &s, std::string::iterator &it) const;
-  PalindromeInfo Expand(std::string::iterator &f_it,
-                        std::string::reverse_iterator &r_it,
-                        std::string &s,
-                        std::size_t initial_length) const;
+public:
+	static std::string longestPalindrome(std::string s);
 };
 
-std::string Solution::longestPalindrome(std::string s) const {
-  return LongestPalindrome(s);
+std::string Solution::longestPalindrome(std::string s)
+{
+	return LongestPalindrome(s);
 }
 
-std::string Solution::LongestPalindrome(std::string &s) const {
-  PalindromeInfo info;
-  for (auto it = s.begin(); it != s.end(); ++it) {
-    auto even_info = TryEven(s, it);
-    if (even_info.length > info.length) {
-      info = even_info;
-    }
-
-    auto odd_info = TryOdd(s, it);
-    if (odd_info.length > info.length) {
-      info = odd_info;
-    }
-  }
-  return std::string(info.begin, info.end);
-}
-
-PalindromeInfo Solution::TryEven(std::string &s, std::string::iterator &it) const {
-  std::string::iterator f_it(it);
-  ++f_it;
-  if (f_it == s.end()) {
-    return {1, it, f_it};
-  }
-
-  std::string::reverse_iterator r_it(f_it);
-  assert(r_it != s.rend());
-  assert(*r_it == *it);
-  if (*f_it != *r_it) {
-    return {1, it, f_it};
-  }
-
-  return Expand(f_it, r_it, s, 2);
-}
-
-PalindromeInfo Solution::TryOdd(std::string &s, std::string::iterator &it) const {
-  std::string::iterator f_it(it);
-  std::advance(f_it, 2);
-  if (f_it == s.end()) {
-    return {1, it, f_it};
-  }
-
-  std::string::reverse_iterator r_it(f_it);
-  assert(r_it != s.rend());
-  ++r_it;
-  assert(*r_it == *it);
-  if (*f_it != *r_it) {
-    return {1, it, f_it};
-  }
-
-  return Expand(f_it, r_it, s, 3);
-}
-
-PalindromeInfo Solution::Expand(std::string::iterator &f_it,
-                                std::string::reverse_iterator &r_it,
-                                std::string &s,
-                                std::size_t initial_length) const {
-  std::size_t length = 0;
-  for (length = 0; (f_it != s.end()) && (r_it != s.rend()) && (*f_it == *r_it); length += 2) {
-    ++f_it;
-    ++r_it;
-  }
-
-  return {initial_length + length, f_it, (++r_it).base()};
-}
-
-int main() {
-  std::cout << Solution().longestPalindrome("abcddc") << std::endl;
-  return 0;
+int main(int argc, char* argv[])
+{
+	std::cout << Solution::longestPalindrome("bb") << std::endl;
+	return 0;
 }

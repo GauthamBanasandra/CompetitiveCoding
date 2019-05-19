7915b02f552563c19b272b03fc3d32ade1658dca
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-05-19 22:41:24 +0530

    [Codechef] Add "Misha and nice sets" - TLE

--- Codechef/Compete/2019/May cook-off/GCDSET/Misha and nice sets.cpp
+++ Codechef/Compete/2019/May cook-off/GCDSET/Misha and nice sets.cpp
@@ -1,15 +1,26 @@
-// WA
+// TLE
 
 #include <iostream>
 #include <cmath>
 
 using ull = long long;
 
-ull CountNiceSetSize(const ull l, const ull r, const ull g)
+ull CountNiceSetSize(const ull l, const ull r, ull g)
 {
-	const auto a = static_cast<ull>(std::ceil(l / static_cast<double>(g))) * g;
-	const auto tn = static_cast<ull>(std::floor(r / static_cast<double>(g))) * g;
-	return (tn - a) / g + 1;
+	ull count = 0;
+	const auto temp = g;
+	while (g <= r)
+	{
+		const auto a = static_cast<ull>(std::ceil(l / static_cast<double>(g))) * g;
+		const auto tn = static_cast<ull>(std::floor(r / static_cast<double>(g))) * g;
+		count += (tn - a) / g + 1;
+		g *= temp;
+		if (temp == 1)
+		{
+			break;
+		}
+	}
+	return count;
 }
 
 int main(int argc, char* argv[])

1b9960d79954a19a81b2553284ea8393232e725f
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-05-22 23:43:38 +0530

    [Leetcode] Add documentation
    * Added documentation to "Best time to buy and sell stock"

--- Leetcode/Practice/Easy/121/Best time to buy and sell stock.cpp
+++ Leetcode/Practice/Easy/121/Best time to buy and sell stock.cpp
@@ -8,16 +8,23 @@ int GetMaxProfit(const std::vector<int>& prices)
 		return 0;
 	}
 
+	// Assume that the first one is the one with least cost
 	auto min_price = prices.front();
+	// Profit can never be negative
 	auto max_profit = 0;
 
 	for (auto i = 1, len = static_cast<int>(prices.size()); i < len; ++i)
 	{
 		const auto profit = prices[i] - min_price;
+		// Profit being negative implies that prices[i] is a better
+		// minimum than min_price that could give better profits
+		// for the upcoming prices
 		if (profit < 0)
 		{
 			min_price = prices[i];
 		}
+		// Profit is positive so, check if it's the best profit
+		// so far and update
 		else if (profit > max_profit)
 		{
 			max_profit = profit;

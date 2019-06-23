b8ec048e79adea3b55939b2ec0bb6cd45dd23fea
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-06-23 19:01:47 +0530

    [UVa] Add "Is there a second way left"

--- UVa/Graph/Minimum spanning tree/Variants/10462/Is there a second way left.cpp
+++ UVa/Graph/Minimum spanning tree/Variants/10462/Is there a second way left.cpp
@@ -1,7 +1,6 @@
-// WA
-
 #include <algorithm>
 #include <cassert>
+#include <ios>
 #include <iostream>
 #include <limits>
 #include <unordered_map>
@@ -74,6 +73,7 @@ std::size_t ufds::num_components() {
 }
 } // namespace ds
 
+namespace uva_10462 {
 using Edge = std::pair<size_t, size_t>;
 
 class Network {
@@ -83,7 +83,7 @@ public:
   Network(const size_t num_nodes, std::vector<std::pair<Edge, int>> &edge_list)
       : num_nodes_(num_nodes), edge_list_(edge_list) {}
 
-  std::pair<Status, int> GetSecondBestCost();
+  std::pair<Status, int> GetSecondBestCost() const;
 
 private:
   std::pair<bool, int> GetMSTCost() const;
@@ -92,21 +92,21 @@ private:
   std::vector<std::pair<Edge, int>> &edge_list_;
 };
 
-std::pair<Network::Status, int> Network::GetSecondBestCost() {
+std::pair<Network::Status, int> Network::GetSecondBestCost() const {
   assert(num_nodes_ > 0);
 
   std::sort(edge_list_.begin(), edge_list_.end(),
             [](const std::pair<Edge, int> &e1, const std::pair<Edge, int> &e2)
                 -> bool { return e1.second < e2.second; });
 
-  std::vector<Edge> mst;
+  std::vector<size_t> mst;
   mst.reserve(num_nodes_ - 1);
 
   ds::ufds set(num_nodes_);
-  for (const auto &item : edge_list_) {
-    const auto &[u, v] = item.first;
+  for (size_t i = 0, len = edge_list_.size(); i < len; ++i) {
+    auto &[u, v] = edge_list_[i].first;
     if (!set.is_same_set(u, v)) {
-      mst.emplace_back(u, v);
+      mst.emplace_back(i);
       set.union_set(u, v);
     }
   }
@@ -115,31 +115,15 @@ std::pair<Network::Status, int> Network::GetSecondBestCost() {
     return {Status::kNoWay, infinity};
   }
 
-  const auto remove_edge = [this](const size_t u,
-                                  const size_t v) -> std::pair<size_t, int> {
-    const auto len = edge_list_.size();
-    for (size_t i = 0; i < len; ++i) {
-      auto &[edge, cost] = edge_list_[i];
-      const auto temp_cost = cost;
-      if (edge.first == u && edge.second == v) {
-        cost = infinity;
-      }
-      return {i, temp_cost};
-    }
-    return {len, infinity};
-  };
-
   auto min_cost = infinity;
-  for (const auto &[u, v] : mst) {
-    const auto &[removed_idx, cost] = remove_edge(u, v);
-    assert(removed_idx != edge_list_.size());
-
+  for (auto &i : mst) {
+    const auto cost = edge_list_[i].second;
+    edge_list_[i].second = infinity;
     auto [is_possible, mst_cost] = GetMSTCost();
-    edge_list_[removed_idx].second = cost;
+    edge_list_[i].second = cost;
     if (!is_possible) {
       continue;
     }
-
     min_cost = std::min(min_cost, mst_cost);
   }
   return {(min_cost == infinity ? Status::kNoSecondWay : Status::kIsPossible),
@@ -147,15 +131,15 @@ std::pair<Network::Status, int> Network::GetSecondBestCost() {
 }
 
 std::pair<bool, int> Network::GetMSTCost() const {
-  std::sort(edge_list_.begin(), edge_list_.end(),
-            [](const std::pair<Edge, int> &e1, const std::pair<Edge, int> &e2)
-                -> bool { return e1.second < e2.second; });
-
   auto min_cost = 0;
   ds::ufds set(num_nodes_);
   for (const auto &item : edge_list_) {
     const auto &[u, v] = item.first;
     const auto cost = item.second;
+    if (cost == infinity) {
+      continue;
+    }
+
     if (!set.is_same_set(u, v)) {
       min_cost += cost;
       set.union_set(u, v);
@@ -163,10 +147,13 @@ std::pair<bool, int> Network::GetMSTCost() const {
   }
   return {set.num_components() == 1, min_cost};
 }
+} // namespace uva_10462
 
 int main(int argc, char *argv[]) {
+  std::ios::sync_with_stdio(false);
+
   size_t t, num_nodes, num_edges;
-  std::vector<std::pair<Edge, int>> edge_list;
+  std::vector<std::pair<uva_10462::Edge, int>> edge_list;
 
   std::cin >> t;
   for (size_t i = 1; i <= t; ++i) {
@@ -181,17 +168,20 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Case #" << i << " : ";
     auto [status, second_best_cost] =
-        Network(num_nodes, edge_list).GetSecondBestCost();
+        uva_10462::Network(num_nodes, edge_list).GetSecondBestCost();
     switch (status) {
-    case Network::Status::kNoWay:
+    case uva_10462::Network::Status::kNoWay:
       std::cout << "No way" << std::endl;
       break;
-    case Network::Status::kNoSecondWay:
+
+    case uva_10462::Network::Status::kNoSecondWay:
       std::cout << "No second way" << std::endl;
       break;
-    case Network::Status::kIsPossible:
+
+    case uva_10462::Network::Status::kIsPossible:
       std::cout << second_best_cost << std::endl;
       break;
+
     default:
       assert(false);
     }
---
+++ UVa/Graph/Minimum spanning tree/Variants/10462/run.cmd
@@ -0,0 +1,7 @@
+REM .build\Debug\Is_there_a_second_way_left.exe < "Graph\Minimum spanning tree\Variants\10462\Is there a second way left.txt"
+REM .build\Debug\Is_there_a_second_way_left.exe < "Graph\Minimum spanning tree\Variants\10462\Is there a second way left1.txt" | clip
+REM .build\Debug\Is_there_a_second_way_left.exe < "Graph\Minimum spanning tree\Variants\10462\Is there a second way left2.txt" | clip
+REM .build\Debug\Is_there_a_second_way_left.exe < "Graph\Minimum spanning tree\Variants\10462\Is there a second way left3.txt" | clip
+REM .build\Debug\Is_there_a_second_way_left.exe < "Graph\Minimum spanning tree\Variants\10462\Is there a second way left4.txt" | clip
+REM .build\Debug\Is_there_a_second_way_left.exe < "Graph\Minimum spanning tree\Variants\10462\Is there a second way left5.txt" | clip
+.build\Debug\Is_there_a_second_way_left.exe < "Graph\Minimum spanning tree\Variants\10462\Is there a second way left6.txt" | clip

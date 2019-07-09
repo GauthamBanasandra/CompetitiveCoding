a66b81ba8fd754bd5bc2de8dfb1e5981f3c525d5
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-07-09 23:06:01 +0530

    [Hackerrank] Add "Roads and libraries"

--- Hackerrank/Practice/Graph_theory/Roads and libraries/Roads and libraries.cpp
+++ Hackerrank/Practice/Graph_theory/Roads and libraries/Roads and libraries.cpp
@@ -1,6 +1,6 @@
-// WA
-
+#include <algorithm>
 #include <iostream>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -12,6 +12,7 @@ public:
   bool is_same_set(std::size_t i, std::size_t j);
   void union_set(std::size_t i, std::size_t j);
   std::size_t num_components();
+  std::unordered_map<size_t, size_t> components_size();
 
   std::size_t size;
   std::vector<std::size_t> parent_index;
@@ -66,26 +67,44 @@ std::size_t ufds::num_components() {
   }
   return unique.size();
 }
+
+std::unordered_map<size_t, size_t> ufds::components_size() {
+  std::unordered_map<size_t, size_t> size;
+  for (size_t i = 0; i < this->size; ++i) {
+    ++size[find_set(i)];
+  }
+  return size;
+}
 } // namespace ds
 
+namespace RoadsAndLibraries {
 using ll = long long;
 
 ll GetTotalCost(const size_t num_cities,
                 const std::vector<std::pair<size_t, size_t>> &roads,
                 const int cost_road, const int cost_library) {
-  ll total_cost{static_cast<int>(num_cities) * cost_library};
-  ds::ufds set(num_cities);
+  ds::ufds set{num_cities};
   for (const auto &[u, v] : roads) {
     if (!set.is_same_set(u, v)) {
-      const auto new_cost = total_cost - cost_library + cost_road;
-      if (new_cost < total_cost) {
-        total_cost = new_cost;
-        set.union_set(u, v);
-      }
+      set.union_set(u, v);
     }
+  }
+
+  ll total_cost{0};
+  // We would need to analyze for each component
+  // In each component, any node is reachable from any other node
+  // If the size of a component is s, the number of edges in its tree would be
+  // s-1 So, we either have a library built for each node in the component,
+  // costing s * cost_library Or we build s-1 roads with one library, costing
+  // (s-1) * cost_library + cost_library
+  for (const auto &[component, size] : set.components_size()) {
+    total_cost += size == 1 ? cost_library
+                            : std::min(size * cost_library,
+                                       (size - 1) * cost_road + cost_library);
   }
   return total_cost;
 }
+} // namespace RoadsAndLibraries
 
 int main(int argc, char *argv[]) {
   auto q{0}, cost_road{0}, cost_library{0};
@@ -101,7 +120,9 @@ int main(int argc, char *argv[]) {
       --roads[i].first;
       --roads[i].second;
     }
-    std::cout << GetTotalCost(n, roads, cost_road, cost_library) << std::endl;
+    std::cout << RoadsAndLibraries::GetTotalCost(n, roads, cost_road,
+                                                 cost_library)
+              << std::endl;
   }
   return 0;
 }

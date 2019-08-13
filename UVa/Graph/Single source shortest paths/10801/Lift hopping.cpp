fdebf58ee4050221ff1c5ca5898363e85ae7517c
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-08-13 23:06:52 +0530

    [UVa] Add "Lift hopping"

--- UVa/Graph/Single source shortest paths/10801/Lift hopping.cpp
+++ UVa/Graph/Single source shortest paths/10801/Lift hopping.cpp
@@ -1,5 +1,7 @@
-// WA
+// WA - Though it passed on UVa OJ, it failed for a couple of cases on uDebug
 
+#include <algorithm>
+#include <ios>
 #include <iostream>
 #include <limits>
 #include <queue>
@@ -8,6 +10,7 @@
 #include <tuple>
 #include <vector>
 
+namespace uva_10801 {
 const auto infinity = std::numeric_limits<int>::max();
 
 class Scheduler {
@@ -18,17 +21,24 @@ public:
   int GetMinDestinationTime() const;
 
 private:
+  int source_{-1};
   const int destination_;
+  size_t num_lifts_;
   std::vector<std::vector<std::tuple<int, int, int>>> adj_list_;
 };
 
 Scheduler::Scheduler(const std::vector<std::pair<int, std::vector<int>>> &lifts,
                      const int destination)
-    : destination_(destination) {
+    : destination_(destination), num_lifts_(lifts.size()) {
   adj_list_.resize(100);
-  for (auto lift_id = 0, num_lifts = static_cast<int>(lifts.size());
+  for (auto lift_id = 0, num_lifts = static_cast<int>(lifts.size()),
+            min_start = infinity;
        lift_id < num_lifts; ++lift_id) {
     const auto &[time, floors] = lifts[lift_id];
+    if (floors.front() == 0 && time < min_start) {
+      source_ = lift_id;
+      min_start = time;
+    }
     if (floors.size() < 2) {
       continue;
     }
@@ -50,34 +60,47 @@ struct Comparator {
 };
 
 int Scheduler::GetMinDestinationTime() const {
+  if (source_ == -1) {
+    return infinity;
+  }
+
   std::priority_queue<std::tuple<int, int, int>,
                       std::vector<std::tuple<int, int, int>>, Comparator>
       order;
-  std::vector<int> min_cost(100, infinity);
+  std::vector<std::vector<int>> min_cost(num_lifts_,
+                                         std::vector<int>(100, infinity));
 
-  order.emplace(0, 0, 0);
-  min_cost[0] = 0;
+  order.emplace(0, 0, source_);
+  min_cost[source_][0] = 0;
 
   while (!order.empty()) {
     const auto [current_cost, node, type] = order.top();
     order.pop();
 
-    if (current_cost > min_cost[node]) {
+    if (current_cost > min_cost[type][node]) {
       continue;
     }
 
     for (const auto &[adj_node, adj_cost, adj_type] : adj_list_[node]) {
-      const auto cost = min_cost[node] + adj_cost + (type == adj_type ? 0 : 60);
-      if (cost < min_cost[adj_node]) {
-        min_cost[adj_node] = cost;
+      const auto cost =
+          min_cost[type][node] + adj_cost + (type == adj_type ? 0 : 60);
+      if (cost < min_cost[adj_type][adj_node]) {
+        min_cost[adj_type][adj_node] = cost;
         order.emplace(cost, adj_node, adj_type);
       }
     }
   }
-  return min_cost[destination_];
+  return (*std::min_element(
+      min_cost.begin(), min_cost.end(),
+      [this](const std::vector<int> &a, const std::vector<int> &b) -> bool {
+        return a[destination_] < b[destination_];
+      }))[destination_];
 }
+} // namespace uva_10801
 
 int main(int argc, char *argv[]) {
+  std::ios::sync_with_stdio(false);
+
   std::string line;
   auto num_lifts = 0, destination = 0;
   std::vector<int> times;
@@ -105,8 +128,9 @@ int main(int argc, char *argv[]) {
       lifts[i] = std::make_pair(times[i], floors);
     }
 
-    auto min_time = Scheduler(lifts, destination).GetMinDestinationTime();
-    if (min_time == infinity) {
+    auto min_time =
+        uva_10801::Scheduler(lifts, destination).GetMinDestinationTime();
+    if (min_time == uva_10801::infinity) {
       std::cout << "IMPOSSIBLE" << std::endl;
     } else {
       std::cout << min_time << std::endl;
--- UVa/Graph/Single source shortest paths/10801/run.cmd
+++ UVa/Graph/Single source shortest paths/10801/run.cmd
@@ -1,1 +1,4 @@
-.build\Debug\Lift_hopping.exe < "Graph\Single source shortest paths\10801\Lift hopping.txt"
+REM .build\Debug\Lift_hopping.exe < "Graph\Single source shortest paths\10801\Lift hopping.txt"
+REM .build\Debug\Lift_hopping.exe < "Graph\Single source shortest paths\10801\Lift hopping1.txt" | clip
+REM .build\Debug\Lift_hopping.exe < "Graph\Single source shortest paths\10801\Lift hopping2.txt" | clip
+.build\Debug\Lift_hopping.exe < "Graph\Single source shortest paths\10801\Lift hopping3.txt" | clip

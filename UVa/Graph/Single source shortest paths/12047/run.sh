ee1c3e9a838287380529111ea7c139b3e65a7470
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-08-20 10:30:02 +0530

    [UVa] Add "Highest paid toll" - WA
    * Debugging

---
+++ UVa/Graph/Single source shortest paths/12047/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#cmake-build-debug/Highest_paid_toll < "Graph/Single source shortest paths/12047/Highest paid toll.txt"
+#cmake-build-debug/Highest_paid_toll < "Graph/Single source shortest paths/12047/Highest paid toll2.txt"
+cmake-build-debug/Highest_paid_toll < "Graph/Single source shortest paths/12047/Highest paid toll2.debug.txt"

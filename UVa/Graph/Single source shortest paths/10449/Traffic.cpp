4859881f21bd3c62f72fdaecd5fa3d468aacb740
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-08-22 23:02:13 +0530

    [UVa] Add "Traffic" - WIP

--- UVa/Graph/Single source shortest paths/10449/Traffic.cpp
+++ UVa/Graph/Single source shortest paths/10449/Traffic.cpp
@@ -1,4 +1,4 @@
-// WA
+// WIP
 
 #include <algorithm>
 #include <iostream>
@@ -7,7 +7,7 @@
 #include <vector>
 
 using Node = size_t;
-using Cost = int;
+using Cost = long long;
 
 const auto infinity = std::numeric_limits<Cost>::max();
 
@@ -20,12 +20,13 @@ public:
 
 private:
   const size_t num_nodes_;
+  std::vector<Cost> min_cost_;
   std::vector<std::tuple<Node, Node, Cost>> edge_list_;
 };
 
 Selector::Selector(const size_t num_nodes, const std::vector<Cost> &business,
                    const std::vector<std::pair<Node, Node>> &edge_list)
-    : num_nodes_(num_nodes + 1) {
+    : num_nodes_(num_nodes + 1), min_cost_(num_nodes + 1, infinity) {
   edge_list_.resize(edge_list.size());
   std::transform(edge_list.begin(), edge_list.end(), edge_list_.begin(),
                  [&business](const std::pair<Node, Node> &edge)
@@ -34,19 +35,28 @@ Selector::Selector(const size_t num_nodes, const std::vector<Cost> &business,
                    const auto value = business[v - 1] - business[u - 1];
                    return {u, v, value * value * value};
                  });
+
+  min_cost_[1] = 0;
+  for (size_t i = 0; i < num_nodes_ - 1; ++i) {
+    for (const auto &[u, v, cost] : edge_list_) {
+      min_cost_[v] = min_cost_[u] == infinity
+                         ? cost
+                         : std::min(min_cost_[v], min_cost_[u] + cost);
+    }
+  }
 }
 
 Cost Selector::GetMinTotalCost(const Node destination) {
-  std::vector<Cost> min_cost(num_nodes_, infinity);
-  min_cost[1] = 0;
-  for (size_t i = 0; i < num_nodes_ - 1; ++i) {
-    for (const auto &[u, v, cost] : edge_list_) {
-      min_cost[v] = min_cost[u] == infinity
-                        ? cost
-                        : std::min(min_cost[v], min_cost[u] + cost);
+  for (const auto &[u, v, cost] : edge_list_) {
+    if (min_cost_[u] == infinity && u == destination) {
+      return infinity;
+    }
+
+    if (min_cost_[v] > min_cost_[u] + cost && v == destination) {
+      return infinity;
     }
   }
-  return min_cost[destination];
+  return min_cost_[destination];
 }
 
 int main(int argc, char *argv[]) {

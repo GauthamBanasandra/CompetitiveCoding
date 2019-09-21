ef9c6b53573fb1182f83241c9b91bea775e3903d
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-09-21 12:14:39 +0530

    [UVa] Add "Arbitrage II" - TLE

---
+++ UVa/Graph/All pairs shortest paths/436/run.cmd
@@ -0,0 +1,3 @@
+REM java -Dfile.encoding=UTF-8 -classpath ".\Graph\All pairs shortest paths\436\out\production\Arbitrage II" Main < ".\Graph\All pairs shortest paths\436\Arbitrage II.txt"
+java -Dfile.encoding=UTF-8 -classpath ".\Graph\All pairs shortest paths\436\out\production\Arbitrage II" Main < ".\Graph\All pairs shortest paths\436\Arbitrage II.1.txt" | clip
+REM java -Dfile.encoding=UTF-8 -classpath ".\Graph\All pairs shortest paths\436\out\production\Arbitrage II" Main < ".\Graph\All pairs shortest paths\436\Arbitrage II.2.txt" | clip
---
+++ UVa/Graph/All pairs shortest paths/436/src/Main.java
@@ -0,0 +1,116 @@
+// TLE
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Edge {
+    public Edge(String from, BigDecimal cost, String to) {
+        this.from = from;
+        this.cost = cost;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    private String from;
+    private String to;
+    private BigDecimal cost;
+}
+
+class Arbitrage {
+    public Arbitrage(int numNodes, String[] nodes, Edge[] edgeList) {
+        this.numNodes = numNodes;
+        assert this.numNodes == nodes.length;
+
+        adjacencyMatrix = new BigDecimal[this.numNodes][this.numNodes];
+
+        HashMap<String, Integer> nodeIdx = new HashMap<>();
+        for (int i = 0; i < nodes.length; i++) {
+            nodeIdx.put(nodes[i], i);
+        }
+
+        for (Edge edge : edgeList) {
+            int u = nodeIdx.get(edge.getFrom());
+            int v = nodeIdx.get(edge.getTo());
+
+            adjacencyMatrix[u][v] = edge.getCost();
+//            adjacencyMatrix[v][u] = BigDecimal.ONE.divide(edge.getCost(), divisionScale, RoundingMode.UP);
+        }
+
+        for (int i = 0; i < this.numNodes; i++) {
+            for (int j = 0; j < this.numNodes; j++) {
+                if (i == j) {
+                    adjacencyMatrix[i][j] = BigDecimal.ONE;
+                } else if (adjacencyMatrix[i][j] == null) {
+                    adjacencyMatrix[i][j] = BigDecimal.ZERO;
+                }
+            }
+        }
+    }
+
+    public boolean IsPossible() {
+        for (int k = 0; k < numNodes; k++) {
+            for (int i = 0; i < numNodes; i++) {
+                for (int j = 0; j < numNodes; j++) {
+                    BigDecimal cost = adjacencyMatrix[i][k].multiply(adjacencyMatrix[k][j]);
+                    if (cost.compareTo(adjacencyMatrix[i][j]) > 0) {
+                        adjacencyMatrix[i][j] = cost;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < numNodes; i++) {
+            if (adjacencyMatrix[i][i].compareTo(BigDecimal.ONE) > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private final int numNodes;
+    private BigDecimal[][] adjacencyMatrix;
+    private static final int divisionScale = 19;
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int numNodes, numEdges, t = 0;
+        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while ((numNodes = Integer.parseInt(reader.readLine())) != 0) {
+            String[] nodes = new String[numNodes];
+            for (int i = 0; i < numNodes; i++) {
+                nodes[i] = reader.readLine();
+            }
+
+            numEdges = Integer.parseInt(reader.readLine());
+            Edge[] edges = new Edge[numEdges];
+            for (int i = 0; i < numEdges; i++) {
+                StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+                edges[i] = new Edge(tokenizer.nextToken(), new BigDecimal(tokenizer.nextToken()),
+                        tokenizer.nextToken());
+            }
+            reader.readLine();
+
+            Arbitrage arbitrage = new Arbitrage(numNodes, nodes, edges);
+            System.out.printf("Case %d: %s\n", ++t, (arbitrage.IsPossible() ? "Yes" : "No"));
+        }
+    }
+}

593f90f75716875cf53bbae72eaf4a29ec8be206
GauthamBanasandra <gautham.bangalore@gmail.com>
2019-10-05 22:47:34 +0530

    [CI] Fix build failure

--- UVa/Data structures and libraries/Linear DS builtin lib/C++ STL algorithm/10107/What is the median.cpp
+++ UVa/Data structures and libraries/Linear DS builtin lib/C++ STL algorithm/10107/What is the median.cpp
@@ -1,27 +1,29 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 #define MAX_N 10010
 
 int main(int argc, char *argv[]) {
-    std::ios::sync_with_stdio(false);
+  std::ios::sync_with_stdio(false);
 
-    std::vector<long long> list(MAX_N);
-    auto n = 0;
+  std::vector<long long> list(MAX_N);
+  auto n = 0;
 
-    do {
-        std::cin >> list[n++];
-        std::nth_element(list.begin(), list.begin() + (n >> 1), list.begin() + n);
-        if (!(n & 1)) {
-            std::nth_element(list.begin(), list.begin() + (n >> 1) - 1, list.begin() + n);
-        }
+  do {
+    std::cin >> list[n++];
+    std::nth_element(list.begin(), list.begin() + (n >> 1), list.begin() + n);
+    if (!(n & 1)) {
+      std::nth_element(list.begin(), list.begin() + (n >> 1) - 1,
+                       list.begin() + n);
+    }
 
-        const auto median = n & 1 ? list[n >> 1] : list[n >> 1] + list[(n >> 1) - 1] >> 1;
-        if (!std::cin.eof()) {
-            printf("%lld\n", median);
-        }
-    } while (!std::cin.eof());
+    const auto median =
+        n & 1 ? list[n >> 1] : list[n >> 1] + list[(n >> 1) - 1] >> 1;
+    if (!std::cin.eof()) {
+      printf("%lld\n", median);
+    }
+  } while (!std::cin.eof());
 
-    return 0;
+  return 0;
 }

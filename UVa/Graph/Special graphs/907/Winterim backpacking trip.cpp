fac991165be741effbd649e3f1a18b1f844576a0
Gautham Banasandra <gautham.bangalore@gmail.com>
2020-01-22 20:34:55 +0530

    [UVa] Add "Winterim backpacking trip"

--- UVa/Graph/Special graphs/907/Winterim backpacking trip.cpp
+++ UVa/Graph/Special graphs/907/Winterim backpacking trip.cpp
@@ -1,35 +1,54 @@
-// WA
-
 #include <algorithm>
+#include <cassert>
+#include <ios>
 #include <iostream>
+#include <limits>
 #include <ostream>
 #include <vector>
 
 namespace uva_907 {
 using Distance = int;
+const auto infinity = std::numeric_limits<Distance>::max();
 
 class Scheduler {
 public:
-  Scheduler(const std::vector<Distance> &camps, size_t days_left);
+  Scheduler(const std::vector<Distance> &distances, size_t days_left);
 
-  Distance GetMinDistance() { return GetMinDistance(0, days_left_); }
+  Distance GetMinMaxDistance() { return GetMinMaxDistance(0, days_left_); }
 
 private:
-  Distance GetMinDistance(size_t i, size_t days_left);
+  Distance GetMinMaxDistance(size_t i, size_t days_left);
+  Distance GetDistance(size_t from, size_t to);
 
-  const std::vector<Distance> &camps_;
+  const std::vector<Distance> &distances_;
   const size_t days_left_;
+  std::vector<Distance> distances_cumulative_;
   std::vector<std::vector<Distance>> memo_;
 };
 
-Scheduler::Scheduler(const std::vector<Distance> &camps, const size_t days_left)
-    : camps_{camps}, days_left_{days_left} {
-  memo_.resize(camps_.size(), std::vector<Distance>(days_left_ + 1, -1));
+Scheduler::Scheduler(const std::vector<Distance> &distances,
+                     const size_t days_left)
+    : distances_{distances}, days_left_{days_left} {
+  const auto len = distances_.size();
+  memo_.resize(len, std::vector<Distance>(days_left_ + 1, -1));
+
+  distances_cumulative_.resize(len);
+  distances_cumulative_.front() = distances_.front();
+  for (size_t i = 1; i < len; ++i) {
+    distances_cumulative_[i] = distances_[i] + distances_cumulative_[i - 1];
+  }
 }
 
-Distance Scheduler::GetMinDistance(const size_t i, const size_t days_left) {
-  if (i >= camps_.size() - 1) {
-    return camps_[i];
+Distance Scheduler::GetMinMaxDistance(const size_t i, const size_t days_left) {
+  const auto len = distances_.size();
+  assert(i < len);
+  if (i == len - 1) {
+    return distances_[i];
+  }
+
+  // Since we can't camp any further, we'll be walking all the way till the end
+  if (days_left == 0) {
+    return GetDistance(i, len - 1);
   }
 
   auto &memo = memo_[i][days_left];
@@ -37,27 +56,40 @@ Distance Scheduler::GetMinDistance(const size_t i, const size_t days_left) {
     return memo;
   }
 
-  if (days_left == 0) {
-    return memo = GetMinDistance(i + 1, days_left) + camps_[i];
+  auto min_max_distance = infinity;
+  for (auto j = i; j < len - 1; ++j) {
+    auto this_camp_distance = GetDistance(i, j);
+    auto next_camp_distance = GetMinMaxDistance(j + 1, days_left - 1);
+    auto max_distance = std::max(this_camp_distance, next_camp_distance);
+    min_max_distance = std::min(min_max_distance, max_distance);
   }
-  auto camp = GetMinDistance(i + 1, days_left - 1);
-  auto no_camp = GetMinDistance(i + 1, days_left) + camps_[i];
-  return memo = std::min(std::max(camp, camps_[i]), no_camp);
+  return memo = min_max_distance;
+}
+
+Distance Scheduler::GetDistance(const size_t from, const size_t to) {
+  assert(from <= to && to < distances_cumulative_.size());
+  auto distance = distances_cumulative_[to];
+  if (from > 0) {
+    distance -= distances_cumulative_[from - 1];
+  }
+  return distance;
 }
 } // namespace uva_907
 
 int main(int argc, char *argv[]) {
+  std::ios::sync_with_stdio(false);
+
   size_t num_camps;
   size_t days_left;
-  std::vector<uva_907::Distance> camps;
+  std::vector<uva_907::Distance> distances;
 
   while (std::cin >> num_camps >> days_left, !std::cin.eof()) {
     ++num_camps;
-    camps.resize(num_camps);
+    distances.resize(num_camps);
     for (size_t i = 0; i < num_camps; ++i) {
-      std::cin >> camps[i];
+      std::cin >> distances[i];
     }
-    std::cout << uva_907::Scheduler(camps, days_left).GetMinDistance()
+    std::cout << uva_907::Scheduler(distances, days_left).GetMinMaxDistance()
               << std::endl;
   }
   return 0;
--- UVa/Graph/Special graphs/907/run.cmd
+++ UVa/Graph/Special graphs/907/run.cmd
@@ -1,2 +1,3 @@
 REM .\.build\Debug\Winterim_backpacking_trip.exe < "Graph\Special graphs\907\Winterim backpacking trip.txt"
-.\.build\Debug\Winterim_backpacking_trip.exe < "Graph\Special graphs\907\Winterim backpacking trip1.txt" | clip
+REM .\.build\Debug\Winterim_backpacking_trip.exe < "Graph\Special graphs\907\Winterim backpacking trip1.txt" | clip
+.\.build\Debug\Winterim_backpacking_trip.exe < "Graph\Special graphs\907\Winterim backpacking trip2.txt" | clip

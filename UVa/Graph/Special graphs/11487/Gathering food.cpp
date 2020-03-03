602c0125748c32ab41d76f57c6b21ec2c31d5518
Gautham Banasandra <gautham.bangalore@gmail.com>
2020-03-03 21:48:45 +0530

    [UVa] Add "Gathering food" - WA

--- UVa/Graph/Special graphs/11487/Gathering food.cpp
+++ UVa/Graph/Special graphs/11487/Gathering food.cpp
@@ -1,16 +1,20 @@
-// WIP
+// WA
 
 #include <iostream>
 #include <map>
+#include <numeric>
+#include <ostream>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
 
+namespace uva_11487 {
 class Explorer {
 public:
   Explorer(std::vector<std::string> &graph, size_t n);
 
-  std::pair<int, int> GetShortestDistance();
+  std::tuple<bool, size_t, size_t> GetShortestDistance();
 
 private:
   void Explore(int p, int q, char fill, char expected,
@@ -35,7 +39,7 @@ std::vector<std::pair<int, int>> Explorer::directions_;
 Explorer::Explorer(std::vector<std::string> &graph, size_t n)
     : graph_{graph}, num_rows_{static_cast<int>(n)}, num_columns_{
                                                          static_cast<int>(n)} {
-  if (!directions_.empty()) {
+  if (directions_.empty()) {
     directions_ = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
   }
 
@@ -50,6 +54,44 @@ Explorer::Explorer(std::vector<std::string> &graph, size_t n)
       }
     }
   }
+}
+
+std::tuple<bool, size_t, size_t> Explorer::GetShortestDistance() {
+  auto len = positions_.size();
+  if (len == 1) {
+    return {true, 0, 0};
+  }
+
+  std::vector<std::map<size_t, size_t>> distances(len);
+  for (size_t i = 1; i < len; ++i) {
+    auto index = i - 1;
+    for (const auto &[x, y] : directions_) {
+      Explore(positions_[index].first + x, positions_[index].second + y,
+              'a' + index, 'A' + i, distances, index, 1);
+    }
+  }
+
+  distances.pop_back();
+  auto is_possible = true;
+  auto sum = std::accumulate(
+      distances.begin(), distances.end(), static_cast<size_t>(0),
+      [&is_possible](const size_t &sum,
+                     const std::map<size_t, size_t> &m) -> size_t {
+        if (m.empty()) {
+          is_possible = false;
+          return 0;
+        }
+        return m.begin()->first + sum;
+      });
+  auto product = std::accumulate(
+      distances.begin(), distances.end(), static_cast<size_t>(1),
+      [](const size_t &product, const std::map<size_t, size_t> &m) -> size_t {
+        if (m.empty()) {
+          return 0;
+        }
+        return m.begin()->second * product;
+      });
+  return {is_possible, sum, product};
 }
 
 void Explorer::Explore(int p, int q, char fill, char expected,
@@ -82,5 +124,27 @@ bool Explorer::IsValid(int x, int y, char fill, char expected) const {
   }
   return true;
 }
+} // namespace uva_11487
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  size_t n, c = 0;
+  std::vector<std::string> grid;
+
+  while (std::cin >> n, n != 0) {
+    ++c;
+    grid.resize(n);
+    for (size_t i = 0; i < n; ++i) {
+      std::cin >> grid[i];
+    }
+
+    auto [is_possible, shortest_path_len, shortest_path_count] =
+        uva_11487::Explorer(grid, n).GetShortestDistance();
+    if (!is_possible) {
+      std::cout << "Case " << c << ": Impossible" << std::endl;
+    } else {
+      std::cout << "Case " << c << ": " << shortest_path_len << " "
+                << shortest_path_count << std::endl;
+    }
+  }
+  return 0;
+}
---
+++ UVa/Graph/Special graphs/11487/run.cmd
@@ -0,0 +1,2 @@
+REM .\.build\Debug\Gathering_food.exe < "Graph\Special graphs\11487\Gathering food.txt"
+.\.build\Debug\Gathering_food.exe < "Graph\Special graphs\11487\Gathering food.1.txt" | clip

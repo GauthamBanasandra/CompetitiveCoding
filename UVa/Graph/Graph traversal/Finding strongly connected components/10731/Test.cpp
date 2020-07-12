f21633564ec9281ae2f37810872ac5d5efb44c68
Gautham Banasandra <gautham.bangalore@gmail.com>
2020-07-12 22:01:18 +0530

    [UVa] Add "Test"

--- UVa/Graph/Graph traversal/Finding strongly connected components/10731/Test.cpp
+++ UVa/Graph/Graph traversal/Finding strongly connected components/10731/Test.cpp
@@ -1,17 +1,17 @@
-// WIP
-
 #include <algorithm>
+#include <ios>
 #include <iostream>
 #include <limits>
 #include <ostream>
 #include <stack>
+#include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
 
 namespace uva_10731 {
-using NodeId = size_t;
-const auto unvisited = std::numeric_limits<NodeId>::max();
+using NodeId = char;
+const auto unvisited = std::numeric_limits<size_t>::max();
 const size_t num_activities = 5;
 
 struct Node {
@@ -21,8 +21,8 @@ struct Node {
 };
 
 struct ActivityResponsePair {
-  std::vector<int> activities;
-  int response{0};
+  std::vector<NodeId> activities;
+  NodeId response{'\0'};
 
   ActivityResponsePair() { activities.resize(num_activities); }
 };
@@ -31,6 +31,7 @@ class ContradictionFinder {
 public:
   explicit ContradictionFinder(
       const std::vector<ActivityResponsePair> &responses);
+  std::unordered_map<NodeId, Node> GetNodes() const;
 
   std::vector<std::vector<char>> Find() const;
 
@@ -39,32 +40,46 @@ private:
   GetNodeCount(const std::vector<ActivityResponsePair> &responses);
 
   void FindScc(NodeId node_id, size_t &current_visit_index,
-               std::vector<Node> &nodes, std::stack<NodeId> &visit_order,
-               std::vector<std::vector<char>> &components) const;
+               std::unordered_map<NodeId, Node> &nodes,
+               std::stack<NodeId> &visit_order,
+               std::vector<std::vector<NodeId>> &components_out) const;
 
-  const size_t num_nodes_;
-  std::vector<std::unordered_set<NodeId>> adj_list_;
+  std::unordered_map<NodeId, std::unordered_set<NodeId>> adj_list_;
 };
 
 ContradictionFinder::ContradictionFinder(
-    const std::vector<ActivityResponsePair> &responses)
-    : num_nodes_{GetNodeCount(responses)} {
-  adj_list_.resize(num_nodes_);
+    const std::vector<ActivityResponsePair> &responses) {
   for (const auto &response : responses) {
-    adj_list_[response.response].insert(response.activities.begin(),
-                                        response.activities.end());
+    for (const auto &activity : response.activities) {
+      if (adj_list_.find(activity) == adj_list_.end()) {
+        adj_list_[activity] = {};
+      }
+      if (response.response != activity) {
+        adj_list_[response.response].insert(activity);
+      }
+    }
   }
+}
+
+std::unordered_map<NodeId, Node> ContradictionFinder::GetNodes() const {
+  std::unordered_map<NodeId, Node> nodes;
+  for (const auto &[node, _] : adj_list_) {
+    nodes[node] = {};
+  }
+  return nodes;
 }
 
 std::vector<std::vector<char>> ContradictionFinder::Find() const {
   size_t current_visit_index{0};
-  std::vector<Node> nodes(num_nodes_);
+  auto nodes = GetNodes();
   std::stack<NodeId> visit_order;
-  std::vector<std::vector<char>> components;
-  components.reserve(num_nodes_);
+  std::vector<std::vector<NodeId>> components;
+  components.reserve(adj_list_.size());
 
-  for (NodeId node_id = 0; node_id < num_nodes_; ++node_id) {
-    FindScc(node_id, current_visit_index, nodes, visit_order, components);
+  for (const auto &[node_id, _] : adj_list_) {
+    if (nodes.at(node_id).visit_index == unvisited) {
+      FindScc(node_id, current_visit_index, nodes, visit_order, components);
+    }
   }
 
   for (auto &component : components) {
@@ -84,18 +99,19 @@ size_t ContradictionFinder::GetNodeCount(
 }
 
 void ContradictionFinder::FindScc(
-    const NodeId node_id, size_t &current_visit_index, std::vector<Node> &nodes,
-    std::stack<NodeId> &visit_order,
-    std::vector<std::vector<char>> &components) const {
-  auto &node = nodes[node_id];
+    const NodeId node_id, size_t &current_visit_index,
+    std::unordered_map<NodeId, Node> &nodes, std::stack<NodeId> &visit_order,
+    std::vector<std::vector<NodeId>> &components_out) const {
+  auto &node = nodes.at(node_id);
   node.visit_index = node.least_visit_index = current_visit_index++;
   node.is_explored = true;
-  visit_order.push(node_id);
+  visit_order.emplace(node_id);
 
-  for (const auto &adj_node_id : adj_list_[node_id]) {
-    auto &adj_node = nodes[adj_node_id];
+  for (const auto &adj_node_id : adj_list_.at(node_id)) {
+    auto &adj_node = nodes.at(adj_node_id);
     if (adj_node.visit_index == unvisited) {
-      FindScc(adj_node_id, current_visit_index, nodes, visit_order, components);
+      FindScc(adj_node_id, current_visit_index, nodes, visit_order,
+              components_out);
     }
     if (adj_node.is_explored) {
       node.least_visit_index =
@@ -104,23 +120,25 @@ void ContradictionFinder::FindScc(
   }
 
   if (node.visit_index == node.least_visit_index) {
-    std::vector<char> component;
+    std::vector<NodeId> component;
     while (true) {
       const auto adj_node_id = visit_order.top();
       visit_order.pop();
-      component.emplace_back(static_cast<int>(adj_node_id) + 'A');
-      nodes[adj_node_id].is_explored = false;
+      component.emplace_back(adj_node_id);
+      nodes.at(adj_node_id).is_explored = false;
 
       if (node_id == adj_node_id) {
         break;
       }
     }
-    components.emplace_back(std::move(component));
+    components_out.emplace_back(std::move(component));
   }
 }
 } // namespace uva_10731
 
 int main(int argc, char *argv[]) {
+  std::ios::sync_with_stdio(false);
+
   char c;
   size_t n{0};
   size_t t{0};
@@ -131,10 +149,10 @@ int main(int argc, char *argv[]) {
     for (size_t i = 0; i < n; ++i) {
       for (size_t j = 0; j < uva_10731::num_activities; ++j) {
         std::cin >> c;
-        responses[i].activities[j] = c - 'A';
+        responses[i].activities[j] = c;
       }
       std::cin >> c;
-      responses[i].response = c - 'A';
+      responses[i].response = c;
     }
 
     if (t++ > 0) {
---
+++ UVa/Graph/Graph traversal/Finding strongly connected components/10731/run.cmd
@@ -0,0 +1,4 @@
+REM out\build\x64-Debug\Debug\Test.exe < "Graph\Graph traversal\Finding strongly connected components\10731\input.txt"
+REM out\build\x64-Debug\Debug\Test.exe < "Graph\Graph traversal\Finding strongly connected components\10731\input.1.txt" | clip
+REM out\build\x64-Debug\Debug\Test.exe < "Graph\Graph traversal\Finding strongly connected components\10731\input.2.txt" | clip
+out\build\x64-Debug\Debug\Test.exe < "Graph\Graph traversal\Finding strongly connected components\10731\input.3.txt" | clip

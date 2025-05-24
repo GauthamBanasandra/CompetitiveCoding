4635a1dc4648c927da3ae190e7652b88f1b7c1bb
Gautham Banasandra <gautham.bangalore@gmail.com>
2025-05-24 21:11:42 +0530

    Use std::find_if_not

--- Leetcode/Explore/Arrays 101/Deleting items from an array/Remove duplicates from sorted array/Remove duplicates from sorted array.cpp
+++ Leetcode/Explore/Arrays 101/Deleting items from an array/Remove duplicates from sorted array/Remove duplicates from sorted array.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -13,8 +14,9 @@ int Solution::removeDuplicates(std::vector<int> &numbers) {
 
   auto i_it = numbers.begin() + 1;
   for (auto j_it = i_it; j_it != numbers.end();) {
-    for (; j_it != numbers.end() && *j_it == *(i_it - 1); ++j_it)
-      ;
+    j_it = std::find_if_not(j_it, numbers.end(), [&i_it](const auto &e) {
+      return e == *(i_it - 1);
+    });
     if (j_it != numbers.end()) {
       std::swap(*i_it++, *j_it++);
     }
